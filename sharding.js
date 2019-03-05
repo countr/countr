97c1afc4e5e5915c9971dca33175f0d4cb733c76
Gleeny <killdore@outlook.com>
2019-03-05 20:49:48 +0100

    Codacy Issues fixed

--- sharding.js
+++ sharding.js
@@ -1,10 +1,10 @@
 // You do not need to use this file at all. 99/100 times, if you host the bot yourself it's for your own guild - sharding is for big bots such as Countr is today. 
 
-const Discord = require('discord.js');
-const config = JSON.parse(require('fs').readFileSync('./config.json', 'utf8'))
+const Discord = require("discord.js");
+const config = JSON.parse(require("fs").readFileSync('./config.json', 'utf8'))
 
-const manager = new Discord.ShardingManager('./app.js', { totalShards: "auto", respawn: true, token: config.token })
+const manager = new Discord.ShardingManager("./app.js", { totalShards: "auto", respawn: true, token: config.token })
 manager.spawn();
-manager.on('launch', async shard => {
+manager.on("launch", async shard => {
     console.log("Shard " + shard.id + " starting.")
 })
