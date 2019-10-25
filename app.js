d66f284eda83ebf2400cae48418106607b5d4985
Promise <glelud@gmail.com>
2019-10-25 22:20:14 +0200

    Core bot files

---
+++ app.js
@@ -0,0 +1,175 @@
+const Discord = require("discord.js"), fs = require("fs"), config = require("./config.json")
+
+const client = new Discord.Client({ messageSweepInterval: 60, disableEveryone: true, disabledEvents: ["TYPING_START"] }), shId = client.shard ? "Shard " + client.shard.id + ":" : "";
+const db = require("database.js")({ client, config }), fails = {};
+
+client.on("ready", async () => {
+  console.log(shId, "Ready as " + client.user.tag);
+  client.user.setPresence({ status: "idle", game: { name: "the loading screen, ready soon™", type: "WATCHING" }})
+
+  client.guilds.forEach(processGuild)
+})
+
+setInterval(async () => {
+  if (!client.guilds.size) return; // client is not ready yet, or have lost connection
+  var name = prefix + "help (" + await db.global.counts() + " counts this week)"
+  var guild = client.guilds.get(config.mainGuild)
+  if (guild) {
+    const {channel, count} = await db.guild(guild.id).get();
+    name = "#" + guild.channels.get(channel).name + " (" + count + " counts so far)"
+  }
+  client.user.setPresence({ status: "online", game: { name, type: "WATCHING" } })
+}, 60000)
+
+// command handler
+const commands = {} // { "command": "path/to/command.js" }
+fs.readdir("./commands/", (err, categories) => {
+  if (err) console.error(err);
+  for (var category of categories) fs.readdir("./commands/" + category, (err, files) => {
+    if (err) console.error(err);
+    for (var file of files) if (file.endsWith(".js")) commands[file.replace(".js", "")] = require("./commands/" + category + "/" + file)
+  })
+})
+
+client.on("message", async message => {
+  if (!message.guild || message.author.id == client.user.id || message.author.discriminator == "0000") return;
+
+  const gdb = db.guild(message.guild.id), {channel, count, user, modules, regex, timeoutrole} = await gdb.get();
+  if (channel == message.channel.id) {
+    // on bigger bots with not enough ram, not all members are loaded in. So if a member is missing, we try to load it in.
+    if (!message.member && message.author.id) try { message.member = await message.guild.fetchMember(message.author.id, true) } catch(e) {}
+
+    if (message.webhookID == null && (disabledGuilds.includes(message.channel.id) || message.author.bot)) return message.delete();
+    if (message.webhookID || (message.content.startsWith("!") && getPermissionLevel(message.member) >= 1) || message.type !== "DEFAULT") return;
+
+    let regexMatches = false;
+    if (regex.length && getPermissionLevel(message.member) == 0) for (var r of regex) if ((new RegExp(r, 'g')).test(message.content)) regexMatches = true;
+
+    if ((!modules.includes("allow-spam") && message.author.id == user) || message.content.split(" ")[0] !== (count + 1).toString() || (!modules.includes("talking") && message.content !== (count + 1).toString()) || regexMatches) {
+      if (timeoutRole.role) {
+        if (!fails[message.guild.id + "/" + message.author.id]) fails[message.guild.id + "/" + message.author.id] = 0;
+        ++fails[message.guild.id + "/" + message.author.id];
+
+        setTimeout(() => --fails[message.guild.id + "/" + message.author.id], timeoutRole.time * 1000)
+
+        if (fails[message.guild.id + "/" + message.author.id] >= timeoutRole.fails) {
+          if (timeoutRole.duration) await gdb.addTimeout(message.guild.id, message.author.id, timeoutRole.duration)
+          try {
+            await message.member.addRole(timeoutRole.role, "User timed out")
+            if (timeoutRole.duration) setTimeout(() => message.member.removeRole(timeoutRole.role, "User no longer timed out"), timeoutRole.duration * 1000)
+          } catch(e) {}
+        }
+      }
+      return message.delete();
+    }
+
+    ++count; gdb.addToCount(message.author.id).then(() => gdb.checkRole(message.author.id, count))
+
+    var msg = message;
+    try {
+      if (modules.includes("webhook")) await message.channel.fetchWebhooks().then(async webhooks => {
+        let webhook = webhooks.find(wh => wh.name == "Countr");
+        if (!webhook) webhook = await message.channel.createWebhook("Countr").catch(() => null);
+
+        if (webhook) {
+          msg = await webhook.send(message.content, { username: message.author.username, avatarURL: message.author.displayAvatarURL.split("?")[0] }) // "".split("?")[0]"" removes the optional "?size=xxx" in the URL
+          message.delete();
+        }
+      });
+      else if (modules.includes("reposting")) await message.channel.send({embed: {
+        description: "<@!" + message.author.id + ">: " + message.content,
+        color: message.member.displayColor || 3553598
+      }}).then(newMsg => { msg = newMsg; message.delete(); })
+    } catch(e) {} // if it doesn't work, we still have the original "msg" value we can pass on further.
+
+    return gdb.doStuffAfterCount(count, message.author.id, msg);
+  }
+  
+  const prefix = await gdb.getPrefix();
+  if (message.content.startsWith(prefix) || message.content.match(`^<@!?${client.user.id}> `)) {
+    // on bigger bots with not enough ram, not all members are loaded in. So if a member is missing, we try to load it in.
+    if (!message.member && message.author.id) try { message.member = await message.guild.fetchMember(message.author.id, true) } catch(e) {}
+
+    const args = message.content.split(" ");
+    if (args[0].match(`^<@!?${client.user.id}>`)) args.shift(); else args[0] = args[0].slice(prefix.length);
+    const command = args.shift().toLowerCase()
+
+    const commandFile = commands[command], permissionLevel = getPermissionLevel(message.member)
+    if (commandFile) {
+      if (permissionLevel < commandFile.permissionRequried) return message.channel.send("❌ You don't have permission! For help type `" + prefix + "help " + command + "`.");
+      if (commandFile.checkArgs(args) !== true) return message.channel.send("❌ " + commandFile.checkArgs(args) + " For help type `" + prefix + "help " + command + "`.");
+      
+      commandFile.run(client, message, args, config, db, permissionLevel)
+    }
+  } else if (message.content.match(`^<@!?${client.user.id}>`)) return message.channel.send("👋 My prefix is `" + prefix + "`, for help type `" + prefix + "help`.");
+})
+
+let getPermissionLevel = (member) => {
+  if (config.admins[0] == member.user.id) return 4;
+  if (config.admins.includes(member.user.id)) return 3;
+  if (member.hasPermission("MANAGE_GUILD")) return 2;
+  if (member.hasPermission("MANAGE_MESSAGES")) return 1;
+  return 0;
+}
+
+let disabledGuilds = [];
+async function processGuild(guild) {
+  disabledGuilds.push(guild.id);
+
+  const gdb = db.guild(message.guild.id);
+  try {
+    const timeouts = await gdb.getTimeouts(), timeout = await gdb.getTimeoutRole(), modules = await gdb.getModules(), countingChannel = await gdb.getChannel(), count = await gdb.getCount();
+    
+    for (var userid in timeouts) try {
+      if (Date.now() > timeouts[userid]) guild.members.get(userid).removeRole(timeout.role, "User no longer timed out");
+      else setTimeout(() => guild.members.get(userid).removeRole(timeout.role), timeouts[userid] - Date.now(), "User no longer timed out")
+    } catch(e) {}
+
+    if (modules.includes("recover")) {
+      const channel = guild.channels.get(countingChannel);
+
+      if (channel) {
+        let messages = await channel.fetchMessages({ limit: 1, after: count.message })
+        if (messages.size > 0) {
+          let botMsg = await channel.send("💢 Making the channel ready for counting, please wait ...")
+          await channel.overwritePermissions(guild.defaultRole, { SEND_MESSAGES: false })
+
+          let processing = true, fail = false;
+          while (processing) {
+            let messages = await channel.fetchMessages({ limit: 100, after: count.message });
+            messages = messages.filter(m => m.id !== botMsg.id);
+            if (messages.size == 0) processing = false;
+            else await channel.bulkDelete(messages).catch(() => fail = true)
+          }
+
+          await channel.overwritePermissions(guild.defaultRole, { SEND_MESSAGES: true })
+          await botMsg.edit("🔰 Channel is ready! Happy counting!")
+          botMsg.delete(15000);
+        }
+      }
+    }
+  } catch(e) {} return disabledGuilds = disabledGuilds.filter(g => g !== guild.id)
+}
+
+client
+  .on("messageDelete", async message => {
+    if (!message.guild || message.author.bot) return;
+
+    const gdb = db.guild(message.guild.id);
+    if (message.channel.id == await gdb.getChannel() && message.id == await gdb.getLastMessage()) return message.channel.send(message.content + " (" + message.author.toString() + ")").then(m => gdb.setLastMessage(m.id)) // resend if the last count got deleted
+  })
+  .on("messageUpdate", async (oldMessage, message) => {
+    if (!message.guild || message.author.bot) return;
+
+    const gdb = db.guild(message.guild.id);
+    if (message.channel.id == await gdb.getChannel() && message.id == await gdb.getLastMessage()) return message.channel.send(oldMessage.content + " (" + message.author.toString() + ")").then(m => message.delete() && db.setLastMessage(m.id)) // resend if the last count git edited
+  })
+  .on("rateLimit", ms => console.log(shId, "Rate limited. [" + ms + "ms]"))
+  .on("disconnect", dc => console.log(shId, "Disconnected:", dc))
+  .on("reconnecting", () => console.log(shId, "Reconnecting..."))
+  .on("resume", replayed => console.log(shId, "Resumed. [" + replayed + " events replayed]"))
+  .on("error", error => console.log(shId, "Unexpected error:", err))
+  .on("warn", warn => console.log(shId, "Unexpected warning:", warn))
+  .login(config.token)
+
+if (config.listKeys) BLAPI.handle(client, config.listKeys, 15);
---
+++ database.js
@@ -0,0 +1,287 @@
+const mongoose = require("mongoose"), Twitter = require("twitter");
+
+module.exports = (client, config) => {
+  mongoose.connect(config.database_uri, { useNewUrlParser: true })
+
+  const twicli = new Twitter(config.twitterIntegration)
+  function tweet(count, week) {
+    if (!(config.twitterIntegration && config.twitterIntegration.enabled)) return;
+    twicli.post("statuses/update", { status: config.twitterIntegration.message.replace("{{COUNT", count).replace("{{WEEK}}", week) })
+  }
+
+  setInterval(() => Global.findOne({}, (err, global) => {
+    if (err) return;
+    if (!global) global = new Global({
+        counts: 0,
+        week: getWeek(new Date())
+    })
+
+    global.counts += addCount; // prevents conflicts
+    addCount = 0;
+
+    if (global.week != getWeek(new Date())) {
+      tweet(global.counts, global.week)
+      global.counts = 1;
+      global.week = getWeek(new Date())
+    }
+
+    global.save()
+  }), 60000)
+
+  return {
+    global: {
+      counts: () => new Promise(async function(resolve, reject) {
+        Global.findOne({}, (err, global) => {
+          if (err) return reject(err);
+          if (!global) global = new Global({
+            counts: 0,
+            week: getWeek(new Date())
+          })
+
+          resolve(global.counts)
+        })
+      })
+    },
+    guild: (gid) => {
+      await cacheGuild(gid);
+      return {
+        get: async () => await cacheGuild(gid),
+
+        set: (key, value) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid][key] = value;
+
+          let guild = await getGuild(gid);
+          guild[key] = savedGuilds[gid][key];
+          await guild.save().then(resolve).catch(reject);
+          updateTopic(gid, client)
+        }),
+
+        addToCount: (user) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid].count += 1;
+          savedGuilds[gid].user = user;
+          
+          if (!savedGuilds[gid].users[user]) savedGuilds[gid].users[user] = 0;
+          savedGuilds[gid].users[user] += 1;
+                
+          let guild = await getGuild(guildid);
+          guild.count = savedGuilds[gid].count;
+          guild.user = savedGuilds[gid].user;
+          guild.users = savedGuilds[gid].users;
+          await guild.save().then(resolve).catch(reject);
+          updateTopic(guildid, client)
+
+          addCount += 1;
+        }),
+
+        doStuffAfterCount: async (count, user, message) => Promise.all([
+          async () => {
+            savedGuilds[gid].message = message.id;
+
+            let guild = await getGuild(guildid);
+            guild.message = savedGuilds[gid].message;
+            await guild.save().then(resolve).catch(reject);
+          }, async () => {
+            let pins = savedGuilds[gid].pins
+            let pin = Object.keys(pins).find(p => (pins[p].mode == "only" && pins[p].count == count) || (pins[p].mode == "each" && count % pins[p].count == 0));
+
+            if (pin) try {
+              if (message.author.bot) message.pin(); // already reposted
+              else if (pins[pin].action == "repost") {
+                message.delete();
+                message.channel.awaitMessages(m => m.author.id == client.user.id && m.content.startsWith(message.content), { max: 1, time: 60000 }).then(msgs => msgs.first().pin()) // it was the last count, so the bot will automatically resend it. (see app.js)
+              } else message.pin();
+            } catch(e) {}
+          }, async () => {
+            let {notifications: notifs, channel} = savedGuilds[gid], needSave = false;
+            for (var ID in notifs) {
+              let notif = notifs[ID];
+              if (notif && ((notifs.mode == "only" && notifs.count == count) || (notifs.mode == "each" && count % notifs.count == 0))) {
+                try {
+                  let guild = client.guilds.get(gid);
+                  guild.members.get(notifs.user).send({embed: {
+                    description: "🎉 " + guild.name + " reached " + count + " total counts!\nThe user who sent it was <@!" + user + ">.\n\n[**→ Click here to jump to the message!**](https://discordapp.com/channels/" + [gid, channel, message.id].join("/") + ")",
+                    color: config.color,
+                    thumbnail: { url: client.users.get(countUser).displayAvatarURL.split("?")[0] },
+                    footer: { text: "Notification ID " + ID + (notif.mode == "each" ? " - Every " + notif.count : "") }
+                  }})
+                } catch(e) {}
+
+                if (notif.mode == "only") {
+                  savedGuilds[gid].notifications[ID] = null;
+                  needSave = true;
+                }
+              }
+            }
+
+            if (needSave) {
+              let guild = await getGuild(gid);
+              guild.notifications = savedGuilds[gid].notifications;
+              guild.save().catch(e => new Error(e))
+            }
+          }
+        ]),
+        
+        setScore: (user, score) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid].users[user] = score;
+              
+          let guild = await getGuild(guildid);
+          guild.users = savedGuilds[gid].users;
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        addRegex: (regex) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid].regex.push(regex);
+
+          let guild = await getGuild(guildid);
+          guild.regex = savedGuilds[gid].regex;
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        removeRegex: (regex) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid].regex = savedGuilds[gid].regex.filter(r => r !== regex);
+        
+          let guild = await getGuild(guildid);
+          guild.regex = savedGuilds[gid].regex;
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        addTimeout: (user, duration) => new Promise(async function(resolve, reject) {
+          savedGuilds[gid].timeouts[user] = Date.now() + duration * 1000;
+        
+          let guild = await getGuild(guildid);
+          guild.timeouts = savedGuilds[gid].timeouts;
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        toggleModule: (m) => new Promise(async function(resolve, reject) {
+          if (savedGuilds[gid].modules.includes(m)) savedGuilds[gid].modules = savedGuilds[gid].modules.filter(str => str !== m);
+          else savedGuilds[gid].modules.push(m);
+
+          let guild = await getGuild(guildid);
+          guild.modules = savedGuilds[gid].modules
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        setNotification: (ID, user, mode, count) => new Promise(async function(resolve, reject) {
+          if (!user) delete savedGuilds[gid].notifications[ID];
+          else savedGuilds[gid].notifications[ID] = { user, mode, count };
+
+          let guild = await getGuild(guildid);
+          guild.notifications = savedGuilds[gid].notifications
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        getNotifications: (user) => {
+          let IDs = {};
+          for (var ID in guild.notifications) {
+            let notif = guild.notifications[ID]
+            if (notif && notif.user == user) IDs[ID] = notif;
+          }
+          return IDs;
+        },
+
+        setRole: (ID, role, mode, count, duration) => new Promise(async function(resolve, reject) {
+          if (!role) delete savedGuilds[guildid].roles[ID];
+          else savedGuilds[guildid].roles[ID] = { role, mode, count, duration };
+
+          let guild = await getGuild(guildid);
+          guild.roles = savedGuilds[guildid].roles
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        editRole: (ID, prop, value) => new Promise(async function(resolve, reject) {
+          savedGuilds[guildid].roles[ID][prop] = value;
+
+          let guild = await getGuild(guildid);
+          guild.roles[ID][prop] = savedGuilds[guildid].roles[ID][prop]
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        setPin: (ID, mode, count, action) => new Promise(async function(resolve, reject) {
+          if (!mode) delete savedGuilds[guildid].pins[ID];
+          else savedGuilds[guildid].pins[ID] = { mode, count, action };
+
+          let guild = await getGuild(guildid);
+          guild.pins = savedGuilds[guildid].pins
+          guild.save().then(resolve).catch(reject);
+        }),
+
+        editPin: (ID, prop, value) => new Promise(async function(resolve, reject) {
+          savedGuilds[guildid].pins[ID][prop] = value;
+
+          let guild = await getGuild(guildid);
+          guild.pins[ID][prop] = savedGuilds[guildid].pins[ID][prop]
+          guild.save().then(resolve).catch(reject);
+        })
+      }
+    }
+  }
+}
+
+const guildObject = {
+  guildid: "", // the guild ID
+  channel: "", // the counting channel ID
+  count: 0, // the current count
+  user: "", // the current count's user
+  modules: [], // the guild's modules
+  notifications: {}, // the guild's users' notifications
+  roles: {}, // the guild's roles
+  topic: "", // the topic
+  message: "", // the current count's ID
+  prefix: "", // the prefix
+  users: {}, // the users' amount of counts
+  timeoutrole: {}, // a role given when the user fails X amount of times within Y seconds (role, time, fails, duration "permanent" or seconds)
+  timeouts: {}, // log how long the users will have the role
+  regex: [], // regex filters, for the talking module
+  pins: {}, // the guild's pin triggers,
+  liveboard: {} // the guild's live leaderboard location (premium)
+}, guildSchema = mongoose.Schema(guildObject, { minimize: false })
+
+const globalSchema = mongoose.Schema({
+  counts: Number, week: Number
+}, { minimize: false })
+
+const Guild = mongoose.model("Guild", guildSchema), Global = mongoose.model("Global", globalSchema);
+
+function updateTopic(gid, client) {
+  return new Promise(function(resolve, reject) {
+    let guild = await cacheGuild(gid);
+    try {
+      let channel = client.guilds.get(gid).channels.get(guild.channel);
+      if (guild.topic == "") channel.setTopic("**Next count:** " + (guild.count + 1)).then(resolve)
+      else if (guild.topic !== "disable") channel.setTopic(guild.topic.replace("{{COUNT}}", (guild.count + 1))).then(resolve)
+    } catch(e) { resolve(e) /* even if it errors, we still want to continue with our code. */ }
+  })
+}
+
+let savedGuilds = {};
+async function cacheGuild(gid) {
+  if (!savedGuilds[gid]) {
+    let guild = await getGuild(gid);
+    savedGuilds[gid] = {};
+    for (var prop in guildObject) savedGuilds[gid][prop] = guild[prop] || guildObject[prop]; // if the guild doesn't have all properties, we give it all properties.
+  }
+  return savedGuilds[gid];
+}
+
+function getGuild(gid) {
+  return new Promise(function(resolve, reject) {
+    Guild.findOne({ guildid: gid }, (err, guild) => {
+      if (err) return reject(err);
+      if (!guild) {
+        let newGuild = new Guild(guildObject);
+        newGuild.guildid = guildid;
+
+        return resolve(newGuild);
+      } else return resolve(guild);
+    })
+  })
+}
+
+function getWeek(d) {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  let weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+  return weekNo;
+}
