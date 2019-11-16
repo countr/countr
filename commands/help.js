71ad625d0347757e4002c8a932a7ade553ee135f
Promise <glelud@gmail.com>
2019-11-16 13:14:08 +0100

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,10 @@ module.exports = {
     "[-all]": "If you include this, it will show all the commands excluding bot-admins-only commands.",
     "[<search ...>]": "Search for a specific command, category or related."
   },
-  examples: [
-    ""
-  ],
+  examples: {
+    "notifyme": "Will give you infomation about the notifyme-command.",
+    "-all add": "Will give you all commands that have \"add\" in their command, description or usage."
+  },
   aliases: ["commands", "?"],
   permissionRequired: 0,
   checkArgs: (args) => {
@@ -23,32 +24,67 @@ module.exports.run = async function(client, message, args, config, db, { permiss
   let search = args.join(" "), commandsFound = [];
   for (var command in allCommands) if (allCommands[command].permissionRequired <= permission && [
     command.includes(search),
+    (prefix + command).includes(search),
     allCommands[command].description.includes(search),
     Object.keys(allCommands[command].usage).includes(search),
-    Object.values(allCommands[command].usage).includes(search)
-  ].includes(true)) items.push({
+    Object.values(allCommands[command].usage).includes(search),
+    Object.values(allCommands[command].examples).includes(search)
+  ].includes(true)) commandsFound.push({
     name: prefix + command,
     value: [
       "**Description:** " + allCommands[command].description,
       "**Permission Level Required:** " + ["None", "Chat Moderators", "Server Administrators", "Bot Support Team", "Bot Owner"][allCommands[command].permissionRequired] + (permission !== permissionLevel ? " \`" + (permissionLevel >= allCommands[command].permissionRequired ? "✅" : "❌") + "\`" : ""),
       "**Usage:** \`" + prefix + command + Object.keys(allCommands[command].usage).map(arg => " " + arg).join("") + "\`",
       "**Arguments:** " + (Object.keys(allCommands[command].usage).map(arg => "\n- \`" + arg + "\`: " + allCommands[command].usage[arg]).join("") || "None."),
-      "**Examples:**" + (Object.keys(allCommands[command].examples).map(ex => "\n- \`" + prefix + command + " " + ex + "\`: " + allCommands[command].examples[ex]).join("") || "None.")
+      "**Examples:** " + (Object.keys(allCommands[command].examples).map(ex => "\n- \`" + prefix + command + " " + ex + "\`: " + allCommands[command].examples[ex]).join("") || "None.")
     ].join("\n"),
-    inline: true
+  inline: false
   })
 
-  message.channel.send({embed: {
+  let pages = Math.ceil(commandsFound.length / 5), page = 1;
+
+  const help = {embed: {
     description: [
-      "`[<x>]` means it is optional to include value, `<x>` means it is required to include value,",
-      "`[x]` means it is optional to include \"x\", `x` means it is required to include \"x\"`.",
-      search.length ? "\n**Search query:** \`" + search + "\`" : ""
+      "`[<val>]` means it is optional to include a value, `<val>` means it is required to include a value,",
+      "`[xyz]` means it is optional to include \"xyz\", `xyz` means it is required to include \"xyz\".",
+      search.length ? "\n**Found **\`" + commandsFound.length + "\`** results with query:** \`" + search + "\`" : "\n**Found **\`" + commandsFound.length + "\`** commands.**"
     ].filter(s => s.length).join("\n"),
     color: config.color,
-    footer: { text: "Requested by " + message.author.tag, iconURL: message.author.displayAvatarURL.split("?")[0] }
-  }}).then(botMsg => {
+    fields: commandsFound.slice(0, 5), // only show the 5 first elements
+    footer: { text: "Requested by " + message.author.tag + (pages > 1 ? " • Page " + page + " of " + pages : ""), icon_url: message.author.displayAvatarURL },
+    timestamp: Date.now()
+  }}
+
+  message.channel.send(help).then(async botMsg => {
+    if (pages > 1) await botMsg.react("♻️") && await botMsg.react("⬅️") && await botMsg.react("➡️");
+    botMsg.react("❌")
     
-  }).catch(() => message.channel.send("❌ An unknown error occoured. Do I have permission? (Embed Links)"))
+    let timedout = false
+    while (!timedout) try {
+      let collected = await botMsg.awaitReactions((_, user) => user.id == message.author.id, { errors: [ "time" ], time: 120000, maxEmojis: 1 })
+      let reaction = collected.first();
+
+      if (reaction.emoji == "♻️") page = 1;
+      else if (reaction.emoji == "⬅️") page -= 1;
+      else if (reaction.emoji == "➡️") page += 1;
+      else if (reaction.emoji == "❌") return botMsg.edit("🔰 Closed by user. Open it again with \`" + prefix + "help\`.", {embed:{}}) && botMsg.clearReactions();
+
+      if (page < 1) page = 1; // if they try accessing a page below page one, we restrict them
+      else if (page > pages) page = pages; // if they try accessing a page that does not exist, we restrict them.
+      
+      reaction.remove(message.author.id);
+      
+      if (JSON.stringify(help.embed.fields) !== JSON.stringify(commandsFound.slice((page - 1) * 5, page * 5))) { // we don't want to excessively rate limit the bot if they try to access a page that does not exist
+        help.embed.fields = commandsFound.slice((page - 1) * 5, page * 5)
+        help.embed.footer.text = "Requested by " + message.author.tag + " • Page " + page + " of " + pages
+        botMsg.edit(help)
+      }
+    } catch(e) { // the timer went out
+      console.log(e)
+      timedout = true
+      return botMsg.edit("⏲️ Timed out. Open it again with \`" + prefix + "help\`.", {embed:{}}) && botMsg.clearReactions();
+    }
+  }).catch(() => message.channel.send("🆘 An unknown error occoured. Do I have permission? (Embed Links, Add Reactions, Manage Messages)"))
 }
 
 const allCommands = {}
