2f36b333817d19465284019d5e334d3941ec9eac
Promise <glelud@gmail.com>
2019-11-18 20:16:04 +0100

    Create addregex command, needs testing

---
+++ commands/addregex.js
@@ -0,0 +1,33 @@
+module.exports = {
+  description: "Add a regex filter for the talking module, filtering unwanted chats.",
+  usage: {
+    "<regex ...>": "The regex you want to filter out of the chat. Get info on how to create a regex here: https://flaviocopes.com/javascript-regular-expressions/#regular-expressions-choices"
+  },
+  examples: {
+    "duck|poop": "Will filter out all messages containing duck and/or poop.",
+    "[A-Z]": "Will filter out all messages with capital letters.",
+    "[A-Ca-cX-Zx-z]": "Will filter out A, B, C, X, Y, Z - regardless if it's capital or not."
+  },
+  aliases: [],
+  permissionRequired: 2,
+  checkArgs: (args) => {
+    return !!args.length;
+  }
+}
+
+module.exports.run = async function(client, message, args, config, gdb, { prefix }) {
+  let regex = args.join(" ");
+  if (!testRegex(regex)) return message.channel.send("❌ Invalid regex. For help, type `" + prefix + "help addregex`")
+
+  gdb.addRegex(regex)
+    .then(() => message.channel.send("✅ Regex `" + regex + "` is now saved."))
+    .catch(e => console.log(e) && message.channel.send("🆘 An unknown database error occoured. Please try again, or contact support."))
+}
+
+function testRegex(regex) {
+  try {
+    new RegExp(regex, "g"); return true;
+  } catch(e) {
+    return false;
+  }
+}
