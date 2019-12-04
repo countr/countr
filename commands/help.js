6f9de4c670f327891a6f58b51bc6cb4919648bc5
Promise <glelud@gmail.com>
2019-12-04 20:11:21 +0100

    Removed unused arg

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   aliases: ["commands", "?"],
   permissionRequired: 0, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
-  checkArgs: (args) => true
+  checkArgs: () => true
 }
 
 const fs = require("fs");
