6f2b66fc7199f3122261dca01124235bc10f4b74
Promise <glelud@gmail.com>
2019-12-08 18:25:36 +0100

    Link command

---
+++ commands/link.js
@@ -0,0 +1,24 @@
+module.exports = {
+  description: "Link a counting channel manually.",
+  usage: {
+    "[<channel>]": "The new counting channel."
+  },
+  examples: {},
+  aliases: [],
+  permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
+  checkArgs: (args) => args.length == 0 || args.length == 1
+}
+
+module.exports.run = async function(client, message, args, config, gdb, prefix, permissionLevel, db) {
+  let channel = message.channel;
+
+  if (args[0]) channel = [
+    message.guild.channels.find(c => c.name == args[0])
+  ].find(c => c)
+
+  if (!channel) return message.channel.send("❌ Invalid channel. For help, type `" + prefix + "help link`")
+
+  gdb.set("channel", channel.id)
+    .then(() => message.channel.send("✅ The channel has been linked! Keep in mind commands inside the counting channel will not work, and these commands has to be outside of the counting channel."))
+    .catch(e => console.log(e) && message.channel.send("🆘 An unknown database error occoured. Please try again, or contact support."))
+}
