f5d044c74b2555fa2bc855897cef8425cd001656
Promise <glelud@gmail.com>
2019-12-25 16:43:30 +0100

    Renamed prefix command

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -7,7 +7,7 @@ module.exports = {
     "c?": "Set the prefix to c?, the help command would then be c?help.",
     "Hey Countr,{{SPACE}}": "Set the prefix to a Google Assistant-like one."
   },
-  aliases: [ "setprefix" ],
+  aliases: [ "prefix", "=prefix" ],
   permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => !!args.length
 }
