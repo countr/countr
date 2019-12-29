82561124792ac50af26367d838cbf2952517b8e7
Promise <glelud@gmail.com>
2019-12-29 15:59:11 +0100

    Removefromscore command

---
+++ commands/removefromscore.js
@@ -0,0 +1,42 @@
+module.exports = {
+  description: "Set a member's score",
+  usage: {
+    "<member(s ...) and/or role(s ...)>": "The member(s) or members of role(s) you want to set the score of",
+    "<score>": "The new score"
+  },
+  examples: {
+    "110090225929191424 9999999": "Will set member with ID 110090225929191424's score to 9999999.",
+    "@Promise#0001 @CountingGods 1337": "Will set Promise#0001's and all members in role Counting Gods' score to 1337."
+  },
+  aliases: [ "-fromscore", "-score" ],
+  permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
+  checkArgs: (args) => args.length >= 2
+}
+
+module.exports.run = async function(client, message, args, config, gdb, prefix, permissionLevel, db) {
+  const removeFromScore = parseInt(args.pop());
+  if (!removeFromScore) return message.channel.send("❌ Invalid score. For help, type `" + prefix + "help removefromscore`")
+
+  const members = [];
+  for (const arg of args) {
+    let search = arg.split("_").join(" "), obj = [
+      message.guild.members.find(m => search == m.user.tag),
+      message.guild.members.get(search.replace("<@", "").replace("!", "").replace(">", "")),
+      message.guild.roles.find(r => r.name == search),
+      message.guild.roles.get(search.replace("<&", "").replace(">", ""))
+    ].find(o => o)
+
+    if (obj) {
+      if (obj.members) obj.members.forEach(m => members.push(m))
+      else members.push(obj)
+    }
+  }
+
+  const removeFromScores = {}
+
+  for (const member of members) removeFromScores[member.id] = -removeFromScore;
+
+  gdb.importScores(removeFromScores, "add")
+    .then(() => message.channel.send("✅ " + (members.length > 1 ? "Score of " + members.length + " members" : "Score of 1 member") + " has been given new values."))
+    .catch(e => console.log(e) && message.channel.send("🆘 An unknown database error occurred. Please try again, or contact support."))
+}
