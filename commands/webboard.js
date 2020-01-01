ba5cd3695ef812d858639044005478ed25d8f622
Promise <glelud@gmail.com>
2020-01-01 22:27:41 +0100

    Update webboard.js

--- commands/webboard.js
+++ commands/webboard.js
@@ -2,11 +2,11 @@ module.exports = {
   description: "Gets a link to the webboard of Countr",
   usage: {},
   examples: {},
-  aliases: ["web", "admininterface", "interface"],
+  aliases: [ "web", "admininterface", "interface", "analytics" ],
   permissionRequired: 0, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => !args.length
 }
 
 module.exports.run = async function(client, message, args, config, gdb, prefix, permissionLevel, db) {
-  message.channel.send("🆗 Your link to the Dashboard is: https://analytics.countr.xyz/viewguild.php/?id=" + message.guild.id);
+  message.channel.send("🆗 Your link to the web scoreboard is: <https://analytics.countr.xyz/viewguild.php/?id=" + message.guild.id + ">");
 }
