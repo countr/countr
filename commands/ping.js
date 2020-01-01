27c6eee81ab8b7f9ae13bea6c33e9f56f898b66a
Promise <glelud@gmail.com>
2020-01-01 12:18:40 +0100

    Remove everything

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (client, message, args, db, permissionLevel, strings, config) => {
-    let botMsg = await message.channel.send("〽 " + strings["PINGING"]);
-    botMsg.edit("🏓 " + strings["PINGED"].replace("{{LATENCY.SERVER}}", "\`" + (botMsg.createdTimestamp - message.createdTimestamp) + "ms\`").replace("{{LATENCY.API}}", "\`"+ Math.round(client.ping) + "ms\`"));
-}
-
-// 0 All, 1 Mods, 2 Admins, 3 Global Admins, 4 First Global Admin
-module.exports.permissionRequired = 0
-module.exports.argsRequired = 0
