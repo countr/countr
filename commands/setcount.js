9be9557e62616cdfdfe04039bcd87b493b9a6214
Promise <glelud@gmail.com>
2020-01-05 10:50:35 +0100

    Downgraded to moderation tool instead

--- commands/setcount.js
+++ commands/setcount.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   examples: {},
   aliases: [ "set", "=", "=count" ],
-  permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
+  permissionRequired: 1, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => args.length == 1
 }
 
