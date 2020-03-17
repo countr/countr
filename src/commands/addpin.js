5f4dd2d2641a7fff73240da3e24e113b015fd7d0
Promise <glelud@gmail.com>
2020-03-17 22:13:40 +0100

    Converted(?) - untested

---
+++ src/commands/addpin.js
@@ -0,0 +1,33 @@
+module.exports = {
+  description: "Add a pintrigger so big milestones gets pinned in chat.",
+  usage: {
+    "<mode: each|only>": "If you use each, it will pin every <count> count. If you use only, it will only pin count <count>.",
+    "<count>": "The count you want to reference in your mode.",
+    "[<action: keep|repost>]": "If you use repost, it will repost the message before pinning it. Default is keep, which does not do this."
+  },
+  examples: {
+    "each 1000 repost": "Will pin every 1000th count after reposting it, including 2000 and 3000 etc.",
+    "only 420": "Will pin the count 1337 as-is."
+  },
+  aliases: [ "+pin", "addpintrigger", "+pintrigger" ],
+  permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
+  checkArgs: (args) => args.length == 2 || args.length == 3
+}
+
+const { generateID } = require("../database.js")
+
+module.exports.run = async function(client, message, args, gdb, strings) {
+  let mode = args[0].toLowerCase();
+  if (!["each", "only"].includes(mode)) return message.channel.send(`❌ ${strings.invalidMode} ${strings.commandHelp}`)
+
+  let count = parseInt(args[1]);
+  if (!count) return message.channel.send(`❌ ${strings.invalidCount} ${strings.commandHelp}`)
+
+  let action = (args[2] || "keep").toLowerCase();
+  if (!["keep", "repost"].includes(action)) return message.channel.send(`❌ ${strings.invalidAction} ${strings.commandHelp}`)
+
+  let { pins } = gdb.get(), id = generateID(Object.keys(pins));
+  return gdb.setPin(id, mode, count, action)
+    .then(() => message.channel.send(`✅ ${strings.savedPintrigger.replace(/{{ID}}/g, id)}`))
+    .catch(e => console.log(e) && message.channel.send(`🆘 ${strings.databaseError}`))
+}
---
+++ src/commands/addregex.js
@@ -0,0 +1,31 @@
+module.exports = {
+  description: "Add a regex filter for the talking module, filtering unwanted messages.",
+  usage: {
+    "<regex ...>": "The regex you want to filter out of the chat. Get info on how to create a regex here: https://flaviocopes.com/javascript-regular-expressions/#regular-expressions-choices"
+  },
+  examples: {
+    "duck|poop": "Will filter out all messages containing duck and/or poop.",
+    "[A-Z]": "Will filter out all messages with capital letters.",
+    "[A-Ca-cX-Zx-z]": "Will filter out A, B, C, X, Y, Z - regardless if it's capital or not."
+  },
+  aliases: [ "+regex", "addfilter", "+filter" ],
+  permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
+  checkArgs: (args) => !!args.length
+}
+
+module.exports.run = async function(client, message, args, gdb, strings) {
+  let regex = args.join(" ");
+  if (!testRegex(regex)) return message.channel.send(`❌ ${strings.invalidRegex} ${strings.commandHelp}`)
+
+  return gdb.addRegex(regex)
+    .then(() => message.channel.send(`✅ ${strings.savedRegex.replace(/{{REGEX}}/g, regex)}`))
+    .catch(e => console.log(e) && message.channel.send(`🆘 ${strings.databaseError}`))
+}
+
+function testRegex(regex) {
+  try {
+    return new RegExp(regex, "g");
+  } catch(e) {
+    return false;
+  }
+}
