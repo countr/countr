d02bf0df746c9ae8cc3b0ec87fb3bd42d63cbef3
Promise <glelud@gmail.com>
2020-03-18 21:54:25 +0100

    Resolver changes

--- src/utils/resolvers.js
+++ src/utils/resolvers.js
@@ -1,20 +1,20 @@
 const idResolver = /[0-9]+/
 
-module.exports.getRole = (argument, { guild }) =>
-  guild.roles.cache.find(r => r.name == argument) ||
-  guild.roles.cache.find(r => r.name.toLowerCase() == argument.toLowerCase()) ||
-  guild.roles.cache.get(argument.match(idResolver)) || null
+module.exports.getRole = (search, guild) =>
+  guild.roles.cache.find(r => r.name == search) ||
+  guild.roles.cache.find(r => r.name.toLowerCase() == search.toLowerCase()) ||
+  guild.roles.cache.get(search.match(idResolver)) || null
 
-module.exports.getMember = (argument, { guild }) =>
-  guild.members.cache.find(m => argument == m.user.tag) ||
-  guild.members.cache.find(m => argument.toLowerCase() == m.user.tag.toLowerCase()) ||
-  guild.members.cache.find(m => argument == m.displayName) ||
-  guild.members.cache.find(m => argument.toLowerCase() == m.displayName.toLowerCase()) ||
-  guild.members.cache.get(argument.match(idResolver)) || null
+module.exports.getMember = (search, guild) =>
+  guild.members.cache.find(m => search == m.user.tag) ||
+  guild.members.cache.find(m => search.toLowerCase() == m.user.tag.toLowerCase()) ||
+  guild.members.cache.find(m => search == m.displayName) ||
+  guild.members.cache.find(m => search.toLowerCase() == m.displayName.toLowerCase()) ||
+  guild.members.cache.get(search.match(idResolver)) || null
 
-module.exports.getChannel = (argument, { guild }) => {
+module.exports.getChannel = (search, guild) => {
   const channels = guild.channels.cache.filter(ch => ch.type == "text" && ch.viewable);
   return false ||
-    channels.find(ch => argument.toLowerCase() == ch.name.toLowerCase()) ||
-    channels.get(argument.match(idResolver))
+    channels.find(ch => search.toLowerCase() == ch.name.toLowerCase()) ||
+    channels.get(search.match(idResolver))
 }
