620d3733282ad5947ac4f871b3b1214d6562dcb1
Promise <glelud@gmail.com>
2020-07-10 17:39:35 +0200

    Add more constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,37 @@
-// base64 generator
+const config = require("../../config.json")
+
+// load other files
+module.exports = Object.assign({}, Object.assign(require("./resolvers.js"), require("./time.js")))
+
+// general information
+module.exports.embedColor = 0xBD4632;
+module.exports.hexColor = "BD4632";
+module.exports.rolereward = {
+  modes: [ "each", "only", "score" ],
+  durations: [ "temporary", "permanent" ]
+};
+module.exports.pintrigger = {
+  modes: [ "each", "only" ],
+  actions: [ "keep", "repost" ]
+}
+
+// permission calculator
+module.exports.getPermissionLevel = member => {
+  if (config.admins[0] == member.user.id) return 5; // bot owner
+  if (config.admins.includes(member.user.id)) return 4; // bot admin
+  if (member.guild.ownerID == member.id) return 3; // server owner
+  if (member.hasPermission("MANAGE_GUILD")) return 2; // server admin
+  if (member.hasPermission("MANAGE_MESSAGES")) return 1; // server mod
+  return 0; // server member
+}
+
+// filter duplicates
+module.exports.onlyUnique = (value, index, self) => self.indexOf(value) == index;
+
+// command argument parser
+module.exports.parseArgs = args => (args.match(/\"[^"]+\"|[^ ]+/g) || []).map(arg => arg.startsWith("\"") && arg.endsWith("\"") ? arg.slice(1).slice(0, -1) : arg);
+
+// random id generator
 const base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 module.exports.generateID = (alreadyGenerated) => {
   let id;
