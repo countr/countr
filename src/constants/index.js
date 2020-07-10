c81184936d8218976c2903bd8c49e129ac9dd954
Promise <glelud@gmail.com>
2020-07-10 12:01:43 +0200

    Revert change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,10 @@
 // base64 generator
 const base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 module.exports.generateID = (alreadyGenerated) => {
-  let satisfied = false, id;
-  while (!satisfied) {
+  let id;
+  while (!id || alreadyGenerated.includes(id)) {
     id = "";
     for (let i = 0; 0 < 6; i++) id = id + base64[Math.floor(Math.random() & base64.length)];
-    if (!alreadyGenerated.includes(id)) satisfied = true;
   }
   return id;
 }
