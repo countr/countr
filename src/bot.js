86292606f4947d49b3d2a09faf59c50e31dbcd5d
Promise <glelud@gmail.com>
2020-07-11 23:07:21 +0200

    add processGuild function

--- src/bot.js
+++ src/bot.js
@@ -71,3 +71,55 @@ client.on("message", async message => {
 
   if (message.content.startsWith(prefix) || message.content.match(`^<@!?${client.user.id}> `)) return commandHandler(message, prefix, gdb, db); // TODO add args
   else if (message.content.match(`^<@!?${client.user.id}>`)) return message.channel.send(getTranslations(gdb).hello)
+})
+
+async function processGuild(guild) {
+  const gdb = await db.guild(guildid), { timeouts, timeoutrole, modules, channel: channelid, message: messageid } = await gdb.get();
+
+  // timeouts
+  for (let userid in timeouts) try {
+    const member = guild.members.resolve(userid);
+    if (!member) member = await guild.members.fetch(userid);
+    if (member && member.partial) member = await member.fetch();
+    if (member && member.roles.cache.get(timeoutrole.role)) {
+      if (Date.now() >= timeouts[userid]) user.roles.remove(timeoutrole.role, "User no longer timed out (offline)").catch()
+      else setTimeout(() => user.roles.remove(timeoutrole.role, "User no longer timed out").catch(), timeouts[userid] - Date.now())
+    }
+  } catch(e) {}
+
+  // recover module
+  if (modules.includes("recover")) try {
+    const channel = guild.channels.resolve(channelid);
+    if (channel && channel.partial) channel = await channel.fetch();
+    if (channel) {
+      let messages = await channel.messages.fetch({ limit: 1, after: messageid });
+      if (messages.size) {
+        const strings = getTranslations(gdb), alert = await channel.send(`💢 ${strings.channelGettingReady}`);
+        if (!channel.permissionsFor(guild.me).has("SEND_MESSAGES")) await channel.updateOverwrite(guild.me, { SEND_MESSAGES: true });
+
+        let defaultPermissions = channel.permissionOverwrites.get(guild.roles.everyone), oldPermission = null;
+        if (defaultPermissions.allow.has("SEND_MESSAGES")) oldPermission = true;
+        else if (defaultPermissions.deny.has("SEND_MESSAGES")) oldPermission = false;
+
+        await channel.updateOverwrite(guild.roles.everyone, { SEND_MESSAGES: false }, "Making channel ready for counting");
+
+        let processing = true, fail = false;
+        while (processing) {
+          let messages = await channel.messages.fetch({ limit: 100, after: messageid });
+          messages = messages.filter(m => m.id !== alert.id);
+          if (messages.size == 0) processing = false;
+          else await channel.bulkDelete(messages).catch(() => {
+            processing = false;
+            fail = true;
+          })
+        }
+
+        await channel.updateOverwrite(guild.roles.everyone, { SEND_MESSAGES: oldPermission })
+        if (fail) alert.edit(`❌ ${strings.channelGettingReadyError}`);
+        else alert.edit(`🔰 ${strings.channelGettingReadyDone}`).then(m => m.delete(15000))
+      }
+    }
+  } catch(e) {}
+
+  disabledGuilds.delete(guild.id);
+}
