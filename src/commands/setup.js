e0abf9c51fe3437dae6509788d9827acf9008ad4
Promise <glelud@gmail.com>
2020-10-19 19:53:41 +0200

    Remove useless property (we only check if it's positive)

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -4,8 +4,7 @@ module.exports = {
   examples: {},
   aliases: [ "autosetup", "quicksetup", "configure" ],
   permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
-  checkArgs: (args) => !args.length,
-  allowInCountingChannel: false
+  checkArgs: (args) => !args.length
 }
 
 module.exports.run = async (message, args, gdb) => {
