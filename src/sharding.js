34d7aa2273044a270fa6fb7650a84d0fbce030e7
Sugden <28943913+NotSugden@users.noreply.github.com>
2020-11-05 21:59:23 +0000

    refactor: use async function

--- src/sharding.js
+++ src/sharding.js
@@ -14,38 +14,28 @@ if (config.port) {
   setInterval(updateBotInfo, 30000);
 
   api.get("/", (_, response) => response.json(botInfo));
-  api.get("/newest", (_, response) => updateBotInfo().then(() => response.json(botInfo)));
+  api.get("/newest", async (_, response) => {
+    const newInfo = await updateBotInfo();
+    return response.json(newInfo);
+  });
   api.listen(config.port);
 }
 
 async function updateBotInfo() {
-  const newBotInfo = {
-    guilds: 0,
-    cachedUsers: 0,
-    users: 0,
-    shards: {},
-    lastUpdate: Date.now()
-  };
-
-  await Promise.all(manager.shards.map(shard => new Promise(resolve => {
-    const newShardInfo = {
-      status: shard.fetchClientValue("ws.status").then(res => newShardInfo.status = res).catch(() => 6),
-      guilds: shard.fetchClientValue("guilds.cache.size").then(res => newShardInfo.guilds = res).catch(() => null),
-      cachedUsers: shard.fetchClientValue("users.cache.size").then(res => newShardInfo.cachedUsers = res).catch(() => null),
-      users: shard.fetchClientValue("guilds.cache").then(guilds => newShardInfo.users = guilds.map(g => g.memberCount).reduce((a, b) => a + b)).catch(() => null)
-    };
-
-    Promise.all(Object.values(newShardInfo)).then(() => {
-      if (newShardInfo.guilds) newBotInfo.guilds += newShardInfo.guilds;
-      if (newShardInfo.users) newBotInfo.users += newShardInfo.users;
-      if (newShardInfo.cachedUsers) newBotInfo.cachedUsers += newShardInfo.cachedUsers;
-  
-      newBotInfo.shards[`${shard.id}`] = newShardInfo;
-      resolve();
-    });
-  })));
-
-  botInfo = newBotInfo;
+  const newBotInfo = await manager.broadcastEval(client => ({
+    status: client.ws.status,
+    guilds: client.guilds.cache.size,
+    cachedUsers: client.users.size,
+    users: client.guilds.cache.reduce((total, guild) => total + guild.memberCount, 0)
+  })).then(results => results.reduce((info, next) => {
+    for (const [key, value] of Object.entries(next)) {
+      info[key] = (info[key] || 0) + value; 
+    }
+    info.shards.push(next);
+    return info;
+  }, { shards: [] }));
+  newBotInfo.lastUpdate = Date.now();
+  return botInfo = newBotInfo;
 }
 
 manager.spawn();
