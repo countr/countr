0f120f89c395570b1b5ba5fd72cab60f05d5f380
Promise <glelud@gmail.com>
2020-11-06 22:31:07 +0100

    Add GIFs explaining each module

--- src/constants/modules.js
+++ src/constants/modules.js
@@ -1,24 +1,29 @@
 module.exports.modules = {
   "allow-spam": {
-    "short": "Allow people to count multiple times in a row."
+    "short": "Allow people to count multiple times in a row.",
+    "image": "https://i.promise.solutions/ISe9n5.gif"
   },
   "recover": {
-    "short": "Remove invalid messages when the bot restarts."
+    "short": "Remove invalid messages when the bot restarts.",
+    "image": "https://i.promise.solutions/xu0imT.gif"
   },
   "reposting": {
     "short": "Repost the message.",
     "incompatible": [
       "webhook"
-    ]
+    ],
+    "image": "https://i.promise.solutions/7HqjEr.gif"
   },
   "talking": {
-    "short": "Allow people to send text after the count."
+    "short": "Allow people to send text after the count.",
+    "image": "https://i.promise.solutions/uTpoA9.gif"
   },
   "webhook": {
     "short": "Repost the message in a webhook.",
     "incompatible": [
       "reposting"
-    ]
+    ],
+    "image": "https://i.promise.solutions/vTQhyU.gif"
   }
 };
 
