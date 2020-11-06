1950d36a3037ca57edeab677fa82958090e99f93
Promise <glelud@gmail.com>
2020-11-06 20:10:39 +0100

    Add new tip

--- src/constants/tips.js
+++ src/constants/tips.js
@@ -5,7 +5,8 @@ module.exports.tips = [
   "Do `{prefix}module recover on` to remove invalid messages after a bot restart.",
   "Do `{prefix}module webhook` to avoid self-deleting.",
   "Don't know how to set up? Do `{prefix}setup`.",
-  "Want people to be able to send a message after the count? Do `{prefix}module talking on`."
+  "Want people to be able to send a message after the count? Do `{prefix}module talking on`.",
+  "Check the documentation out for more ways to customize your server. <https://docs.countr.xyz/>"
 ];
 
 module.exports.generateTip = prefix => `**Tip:** ${this.tips[Math.floor(Math.random() * this.tips.length)].replace(/{prefix}/g, prefix)}`;
