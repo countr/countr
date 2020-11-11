eddeab9b2033b11a11c9398e3b97467ad61c1afd
Promise <glelud@gmail.com>
2020-11-11 13:28:15 +0100

    Remove alias (for new static command)

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
   description: "Get the latency of the bot.",
   usage: {},
   examples: {},
-  aliases: [ "pong", "latency", "uptime" ],
+  aliases: [ "pong", "latency" ],
   permissionRequired: 0, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => !args.length,
   allowInCountingChannel: true
