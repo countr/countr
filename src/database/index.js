d89175b5f56660d129a26ded0119b5f2f998506a
Promise <glelud@gmail.com>
2020-12-19 18:32:01 +0100

    Update database settings

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,10 @@
 const config = require("../../config.json"), mongoose = require("mongoose");
-mongoose.connect(config.database_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(config.database_uri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 
 module.exports = (client) => ({
   guild: require("./guild.js")(client), // guild(guildid)
