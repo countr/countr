83f78203b96cacfca9cc353f20de2982145fb8bf
Promise <glelud@gmail.com>
2020-12-19 19:00:59 +0100

    ok it works now.

--- src/database/guild.js
+++ src/database/guild.js
@@ -33,7 +33,6 @@ const get = (guildid) => new Promise((resolve, reject) => Guild.findOne({ guildi
 }));
 
 const load = async (guildid) => {
-  console.log("loading", guildid)
   let guild = await get(guildid), guildCache = {}, freshGuildObject = JSON.parse(JSON.stringify(guildObject)); // make a fresh one, to not make duplicates across guilds (for example on arrays and objects)
   for (const key in freshGuildObject) guildCache[key] = guild[key] || freshGuildObject[key]; // if there's no value stored in the guild database then we use the default value
   return dbCache.set(guildid, guildCache);
