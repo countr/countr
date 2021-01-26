7daef598a6e3d35cad1fca6bcd746f9fab6d0660
Talal <43641182+Ta1al@users.noreply.github.com>
2021-01-26 20:51:08 +0500

    Catch error when making a channel
    Discord deployed a breaking change to the Create Guild Channel endpoint which partially broke this command;

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -2,7 +2,7 @@ module.exports = {
   description: "Quickly set up a counting channel. You only need to do this once.",
   usage: {},
   examples: {},
-  aliases: [ "autosetup", "quicksetup", "configure" ],
+  aliases: ["autosetup", "quicksetup", "configure"],
   permissionRequired: 2, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => !args.length
 };
@@ -12,14 +12,23 @@ module.exports.run = async (message, _, gdb) => {
   if (channel) {
     const overwrite = await new Promise(resolve => {
       message.channel.send("⚠️ This server has been set up before. If you continue, your old setup would no longer work. Are you sure you want to continue?\nType `yes` or `no` in chat.");
-      message.channel.awaitMessages(m => m.author.id == message.author.id && ["yes", "no"].includes(m.content.toLowerCase()), { max: 1, time: 30000, errors: [ "time" ]})
+      message.channel.awaitMessages(m => m.author.id == message.author.id && ["yes", "no"].includes(m.content.toLowerCase()), { max: 1, time: 30000, errors: ["time"] })
         .then(collection => collection.first().content.toLowerCase() == "yes" ? resolve(true) : resolve(false))
         .catch(() => resolve(false));
     });
     if (!overwrite) return message.channel.send("✴️ New configuration canceled.");
   }
 
-  if (!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send("❌ The bot is missing the `Manage Channels`-permission. Please fix the issue and retry.");
+  const perms = [
+    "MANAGE_CHANNELS",
+    "MANAGE_ROLES", // manage permissions
+    "VIEW_CHANNEL",
+    "SEND_MESSAGES",
+    "MANAGE_MESSAGES",
+    "EMBED_LINKS",
+    "READ_MESSAGE_HISTORY",
+    "MANAGE_WEBHOOKS"
+  ];
 
   const newChannel = await message.guild.channels.create("counting", {
     parent: message.channel.parent,
@@ -27,19 +36,14 @@ module.exports.run = async (message, _, gdb) => {
     permissionOverwrites: [
       {
         id: message.client.user.id,
-        allow: [
-          "MANAGE_CHANNELS",
-          "MANAGE_ROLES", // manage permissions
-          "VIEW_CHANNEL",
-          "SEND_MESSAGES",
-          "MANAGE_MESSAGES",
-          "EMBED_LINKS",
-          "READ_MESSAGE_HISTORY",
-          "MANAGE_WEBHOOKS"
-        ]
+        allow: perms
       }
     ]
+  }).catch(e => {
+    if(e) message.channel.send(`❌ The bot is missing permissions to make a channel. Make sure it has the following permissions:\n\`\`\`css\n${perms.join("\n").replace(/_/g, " ")}\`\`\``);
   });
+
+  console.log(newChannel);
 
   gdb.setMultiple({
     channel: newChannel.id,
