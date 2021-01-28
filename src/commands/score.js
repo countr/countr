a0e5101a0c0a50a56978550c1ddfe44fcb02fad1
Talal <43641182+Ta1al@users.noreply.github.com>
2021-01-28 15:10:50 +0500

    Added alias

--- src/commands/score.js
+++ src/commands/score.js
@@ -4,7 +4,7 @@ module.exports = {
     "[member]": "Another member whose score you want to check"
   },
   examples: {},
-  aliases: [ "rank" ],
+  aliases: [ "rank", "#" ],
   permissionRequired: 0, // 0 All, 1 Mods, 2 Admins, 3 Server Owner, 4 Bot Admin, 5 Bot Owner
   checkArgs: (args) => args.length == 0 || args.length >= 1
 };
