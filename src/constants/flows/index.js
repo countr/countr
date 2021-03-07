ba6e72ea965c4dc89212625af0148eb2e5334b14
Promise <glelud@gmail.com>
2021-03-07 22:25:15 +0100

    Allow more flows, triggers and actions to free users.

--- src/constants/flows/index.js
+++ src/constants/flows/index.js
@@ -1,5 +1,15 @@
-module.exports = Object.assign({
-  limitTriggers: 1,
-  limitActions: 3,
-  limitFlows: 5
-}, require("./flow.js"), require("./propertyTypes.js"), require("./walkthrough.js"));
+const config = require("../../../config.json")
+
+module.exports = Object.assign(config.isPremium ? {
+  limitTriggers: 25,
+  limitActions: 25,
+  limitFlows: 25
+} : {
+  limitTriggers: 3,
+  limitActions: 10,
+  limitFlows: 15
+},
+  require("./flow.js"),
+  require("./propertyTypes.js"),
+  require("./walkthrough.js")
+);
