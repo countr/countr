603a88bb976302174498c0eeb1a6744de89dfb89
Promise <glelud@gmail.com>
2021-03-12 19:09:05 +0100

    Fix for https://github.com/countr/countr/pull/83#pullrequestreview-611014111

--- src/handlers/slashCommands.js
+++ src/handlers/slashCommands.js
@@ -4,6 +4,7 @@ module.exports = async (client, db, shardid) => {
   if (shardid == 0) registerCommands(client).then(() => console.log("Manager: Slash Commands have been registered."));
 
   client.ws.on("INTERACTION_CREATE", async interaction => {
+    if (!interaction.guild_id) return client.api.interactions(interaction.id, interaction.token).callback.post({ data: { type: 4, data: { content: "❌ Countr's commands only works in guilds." } } })
     const
       commandFile = require(`../commands/slash/${interaction.data.name}.js`),
       gdb = await db.guild(interaction.guild_id),
