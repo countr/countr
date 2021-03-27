92d59dc625251520b1c3708516d06f6b4edb8c26
Glenn <me@promise.solutions>
2021-03-27 22:46:45 +0100

    Fix typo

--- src/commands/info.js
+++ src/commands/info.js
@@ -70,7 +70,7 @@ module.exports.run = async (message, _, gdb, { prefix }) => {
             `**Guilds**: \`${message.client.guilds.cache.size}\``,
             `**Users**: \`${message.client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)}\``,
             `**Memory Usage**: \`${memoryUsage}\``,
-            `**Uptime**: \`${msToTime(message.client.uptime)}}\``
+            `**Uptime**: \`${msToTime(message.client.uptime)}\``
           ].join("\n"),
           inline: true
         },
