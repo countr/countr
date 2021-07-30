2b8e1a95401d7917adba97dfbebd86df87d09a3d
Promise <me@promise.solutions>
2021-07-30 22:37:07 +0200

    Initial commit

--- src/commands/slash/count.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  description: "Get the current count.",
-  options: []
-};
-
-module.exports.run = async (send, { gdb }) => {
-  const { count } = gdb.get();
-  send({ content: `ℹ The current count is ${count}.` });
-};
--- src/commands/slash/ping.js
+++
@@ -1,8 +0,0 @@
-const { msToTime } = require("../../constants/index.js");
-
-module.exports = {
-  description: "Get the latency of the bot.",
-  options: []
-};
-
-module.exports.run = async (send, { client }) => send({ content: `🏓 API latency is \`${Math.round(client.ws.ping)}ms\` and my uptime is \`${msToTime(client.uptime)}\`.` });
