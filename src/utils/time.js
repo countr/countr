b074dc63a366b53e6828b6a70fd4f5f835251785
Promise <me@promise.solutions>
2021-08-02 02:15:54 +0200

    Time utility functions

---
+++ src/utils/time.js
@@ -0,0 +1,36 @@
+module.exports = { getWeek, getDateFormatted, msToTime };
+
+function getWeek(d = new Date()) { // https://stackoverflow.com/a/6117889
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  let week = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+
+  return week;
+}
+
+function getDateFormatted(d = new Date()) { // https://stackoverflow.com/a/23593099
+  let month = (d.getMonth() + 1).toString(), day = d.getDate().toString(), year = d.getFullYear();
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+  return [year, month, day].join("-");
+}
+
+function msToTime(ms) { // https://stackoverflow.com/a/19700358
+  const
+    days = Math.floor(ms / 86400000), // 24*60*60*1000
+    daysms = ms % 86400000, // 24*60*60*1000
+    hours = Math.floor(daysms / 3600000), // 60*60*1000
+    hoursms = ms % 3600000, // 60*60*1000
+    minutes = Math.floor(hoursms / 60000), // 60*1000
+    minutesms = ms % 60000, // 60*1000
+    sec = Math.floor(minutesms / 1000);
+
+  let str = "";
+  if (days) str = str + days + "d";
+  if (hours) str = str + hours + "h";
+  if (minutes) str = str + minutes + "m";
+  if (sec) str = str + sec + "s";
+
+  return str || "0s";
+}
