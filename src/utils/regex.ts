7ee19aefe67ab34ec19a4abc40e8b00c1eeb81ed
Promise <me@promise.solutions>
2021-09-16 16:46:50 +0200

    Fix regex util

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,4 +1,13 @@
-import { filters } from "../constants/limits.js";
-import { match } from "time-limited-regular-expressions";
+import limits from "../constants/limits.js";
+import tlre from "time-limited-regular-expressions";
 
-export default match({ limit: filters.timeout });
+const regexTest = tlre({ limit: limits.filters.timeout });
+
+export default async (regex: string, text: string): Promise<boolean | null> => {
+  try {
+    const result = await regexTest(regex, text);
+    return Boolean(result && result[0]);
+  } catch (e) {
+    return null;
+  }
+};
