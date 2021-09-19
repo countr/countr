725529ce2b7a9995479689ccf5b3c7711b000e2d
Promise <me@promise.solutions>
2021-09-19 20:50:28 +0200

    Update text.ts

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,4 +1,8 @@
-export const trim = (string: string, length: number) => {
+export const trim = (string: string, length: number): string => {
   if (string.length <= length) return string;
   else return string.substr(0, length - 3) + "...";
+};
+
+export const capitalizeFirst = (string: string): string => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
