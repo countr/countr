7aa82ecbc13aeeabcdfed5d0f66ff267e2261b65
Promise <me@promise.solutions>
2021-09-20 21:53:20 +0200

    Update text.ts

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -6,3 +6,6 @@ export const trim = (string: string, length: number): string => {
 export const capitalizeFirst = (string: string): string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+export const joinListWithAnd = (list: Array<string>): string => list.concat(list.splice(-2, 2).join(" and ")).join(", ");
+
