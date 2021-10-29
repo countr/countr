e1b6085c199ee603bc765c9efba4f49f20112425
Glenn <glelud@gmail.com>
2021-10-29 12:44:53 +0200

    Add respawnshard command

--- src/commands/mention/_permissions.ts
+++ src/commands/mention/_permissions.ts
@@ -3,6 +3,7 @@ import { CommandPermissionLevel } from "../../constants/permissions";
 const permissions: Record<string, CommandPermissionLevel> = {
   eval: "DEVELOPER",
   ping: "ALL",
+  respawnshard: "SUPPORT",
 };
 
 export default permissions;
