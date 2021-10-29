2a53c286bd8052bfdd65d6d5f30920ab92075a04
Glenn <glelud@gmail.com>
2021-10-29 12:44:27 +0200

    Add ping command

--- src/commands/mention/_permissions.ts
+++ src/commands/mention/_permissions.ts
@@ -1,7 +1,8 @@
 import { CommandPermissionLevel } from "../../constants/permissions";
 
 const permissions: Record<string, CommandPermissionLevel> = {
-  eval: "DEVELOPER"
+  eval: "DEVELOPER",
+  ping: "ALL",
 };
 
 export default permissions;
