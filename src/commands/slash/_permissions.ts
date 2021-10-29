145a9a0507acd52c42240a75fd28c22b08e8297a
Glenn <glelud@gmail.com>
2021-10-29 12:45:27 +0200

    Add permission to flow command

--- src/commands/slash/_permissions.ts
+++ src/commands/slash/_permissions.ts
@@ -1,5 +1,7 @@
 import { CommandPermissionLevel } from "../../constants/permissions";
 
-const permissions: Record<string, CommandPermissionLevel> = {};
+const permissions: Record<string, CommandPermissionLevel> = {
+  flows: "ADMIN",
+};
 
 export default permissions;
