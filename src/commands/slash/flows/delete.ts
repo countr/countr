f5970ebc0df68afd5b2e7b3d306dcd9dfc787b27
Promise <me@promise.solutions>
2021-10-29 23:39:33 +0200

    Add flow delete command

---
+++ src/commands/slash/flows/delete.ts
@@ -0,0 +1,33 @@
+import { SlashCommand } from "../../../types/command";
+import walkthrough from "../../../constants/flows/walkthrough";
+import { flowList } from "../../../constants/autocompleters";
+
+export default {
+  description: "Delete a flow",
+  options: [{
+    type: "STRING",
+    name: "flow",
+    description: "The flow to delete",
+    autocomplete: true,
+    required: true
+  }],
+  autocompletes: {
+    flow: flowList
+  },
+  execute: async (interaction, ephemeralPreference, { flow }: { flow: string }, document, selectedCountingChannel) => {
+    const channel = document.channels.get(selectedCountingChannel || "" /* always defined because requireSelectedCountingChannel is true */);
+
+    if (!channel?.flows.has(flow)) return interaction.reply({
+      content: "❌ That flow doesn't exist.",
+      ephemeral: true
+    }); // this should never trigger considering we're using autocomplete, but we're adding a fallback message just in case
+
+    channel.flows.delete(flow);
+    document.safeSave();
+
+    return interaction.reply({
+      content: `✅ Deleted flow \`${flow}\`.`, ephemeral: ephemeralPreference
+    });
+  },
+  requireSelectedCountingChannel: true,
+} as SlashCommand;
