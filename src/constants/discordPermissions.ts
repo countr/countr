17c73b4135ce35b9c18582fe39d318cccfdae569
Promise <me@promise.solutions>
2021-10-30 11:54:57 +0200

    Add default discord permissions for consistency

---
+++ src/constants/discordPermissions.ts
@@ -0,0 +1,12 @@
+import { PermissionResolvable } from "discord.js";
+
+export const countingChannelPermissions: Array<PermissionResolvable> = [
+  "VIEW_CHANNEL",
+  "SEND_MESSAGES",
+  "MANAGE_MESSAGES",
+  "READ_MESSAGE_HISTORY"
+];
+
+export const countingThreadParentChannelPermissions: Array<PermissionResolvable> = [
+  "SEND_MESSAGES_IN_THREADS"
+];
