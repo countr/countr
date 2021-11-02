578c8e7e2746d9ab7763df1d75fbbacc48ff14cf
Glenn <glenn@not-tech.support>
2021-11-02 12:06:28 +0100

    Use process.env directly

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,13 @@
-import { parse } from "dotenv";
-import { readFileSync } from "fs";
+import { config } from "dotenv";
 import { Config } from "./types/config";
 
-const env = parse(readFileSync("./.env"));
 const isDockerWithMongo = process.env.IS_THIS_DOCKER_WITH_MONGO == "yes";
+config(); // load env variables
 
 export default {
   client: {
-    token: env.BOT_TOKEN,
-    shards: parseInt(env.BOT_SHARDS) || "auto",
+    token: process.env.BOT_TOKEN,
+    shards: parseInt(process.env.BOT_SHARDS || "") || "auto",
     caches: {
       GuildBanManager: 0,
       GuildEmojiManager: 0,
@@ -26,30 +25,30 @@ export default {
       VoiceStateManager: 0
     }
   },
-  database_uri: isDockerWithMongo ? "mongodb://db/countr" : env.DATABASE_URI,
-  isPremium: env.IS_PREMIUM == "true",
+  database_uri: isDockerWithMongo ? "mongodb://db/countr" : process.env.DATABASE_URI,
+  isPremium: process.env.IS_PREMIUM == "true",
 
-  admins: (env.ADMINS || "").split(","),
-  guild: env.GUILD || null,
+  admins: (process.env.ADMINS || "").split(","),
+  guild: process.env.GUILD || null,
 
-  port: parseInt(env.API_PORT),
+  port: parseInt(process.env.API_PORT || ""),
 
   colors: {
-    primary: parseInt(env.COLOR_PRIMARY || "BD4632", 16),
-    success: parseInt(env.COLOR_SUCCESS || "43B581", 16),
-    error: parseInt(env.COLOR_ERROR || "F14747", 16),
-    warning: parseInt(env.COLOR_WARNING || "FAA619", 16),
-    info: parseInt(env.COLOR_INFO || "5865F2", 16),
+    primary: parseInt(process.env.COLOR_PRIMARY || "BD4632", 16),
+    success: parseInt(process.env.COLOR_SUCCESS || "43B581", 16),
+    error: parseInt(process.env.COLOR_ERROR || "F14747", 16),
+    warning: parseInt(process.env.COLOR_WARNING || "FAA619", 16),
+    info: parseInt(process.env.COLOR_INFO || "5865F2", 16),
   },
 
   integration: {
-    webhook_url: env.WEBHOOK_URL || null, // todo
+    webhook_url: process.env.WEBHOOK_URL || null, // todo
   },
 
   access: {
-    enabled: env.ACCESS_ENABLED == "true",
-    interval: parseInt(env.ACCESS_INTERVAL) || 30000,
-    webhook_log: env.ACCESS_WEBHOOK_LOG || null,
+    enabled: process.env.ACCESS_ENABLED == "true",
+    interval: parseInt(process.env.ACCESS_INTERVAL || "") || 30000,
+    webhook_log: process.env.ACCESS_WEBHOOK_LOG || null,
   },
 
   statusPage: "https://uptime.countr.xyz",
