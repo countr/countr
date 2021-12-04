49339f3292c4f58eac2ca54888cd94fd587143e4
Promise <glenn@not-tech.support>
2021-12-04 22:28:47 +0100

    Add `/flows input` command for user input
    Discord planned on adding text area component, which is why this has been postponed so much, but they decided to do modals instead as a replacement)

---
+++ src/commands/slash/flows/input.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction } from "discord.js";
+import { SlashArgRecord } from "../../../handlers/interactions/commands";
+import { SlashCommand } from "../../../types/command";
+import { propertyTypes } from "../../../constants/flows/properties";
+
+export const awaitingInput = new Map<string, (interaction: CommandInteraction, args: SlashArgRecord) => void>();
+
+export default {
+  description: "Add an input to a flow (the flow editor will ask you to run this command)",
+  options: Object.values(propertyTypes).map(prop => prop.input).filter((prop, index, props) => props.indexOf(prop) === index),
+  execute: (interaction, _, args) => {
+    const awaiting = awaitingInput.get([interaction.guildId, interaction.channelId, interaction.user.id].join("."));
+    if (awaiting) return awaiting(interaction, args);
+
+    return interaction.reply({
+      content: "❌ This command is used in the flow editor when editing a property, not by itself.",
+      ephemeral: true,
+    });
+  },
+  disableInCountingChannel: true,
+  requireSelectedCountingChannel: true,
+} as SlashCommand;
