c638adbe29b24d0bb9b8a51916f686ab923ec602
Promise <glenn@not-tech.support>
2021-12-08 20:02:40 +0100

    Add a very garbage garbage collector but it actually works

--- src/handlers/interactions/components.ts
+++ src/handlers/interactions/components.ts
@@ -21,3 +21,26 @@ export default (interaction: SelectMenuInteraction | ButtonInteraction): void =>
     component.callback(interaction);
   }
 };
+
+
+/*
+ * remove map entries that has been there longer than an hour, to avoid indefinite growth and memory leaks.
+ * it would be more optimized to add the timestamp in the map entry itself, but this is easier code-wise as
+ * we just need to do `components.set("key", () => ...)` instead of, for example, doing something like
+ * `components.set("key", { timestamp: Date.now(), callback: () => ... })` in the code. it'll just be messy.
+ */
+
+const timeUntilGarbage = new Map<string, number>();
+
+setInterval(() => {
+  components.forEach((_, key) => {
+    if (!timeUntilGarbage.has(key) && !key.includes("_")) timeUntilGarbage.set(key, Date.now() + 3600000);
+  });
+
+  timeUntilGarbage.forEach((value, key) => {
+    if (value < Date.now()) {
+      components.delete(key);
+      timeUntilGarbage.delete(key);
+    }
+  });
+}, 300000);
