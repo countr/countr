e6c59faf068012f684807354459c2492fbe5a9d1
Glenn <glenn@not-tech.support>
2021-12-09 12:00:30 +0100

    Add unlink command

---
+++ src/commands/slash/channels/unlink.ts
@@ -0,0 +1,43 @@
+import { SlashCommand } from "../../../types/command";
+
+export default {
+  description: "Link an existing channel to Countr",
+  options: [
+    {
+      type: "CHANNEL",
+      name: "channel",
+      description: "The counting channel to unlink",
+      channelTypes: ["GUILD_TEXT", "GUILD_PRIVATE_THREAD", "GUILD_PUBLIC_THREAD"],
+    },
+    {
+      type: "STRING",
+      name: "from_id",
+      description: "The ID of the channel to unlink. Use this if you've already deleted the channel",
+    },
+  ],
+  // eslint-disable-next-line camelcase -- slash command options can't be camel case, we make it camel case though
+  execute: (interaction, ephemeralPreference, { channel: channelId, from_id: fromId }: { channel?: string; from_id?: string; }, document) => {
+    const channel = channelId || fromId;
+    if (!channel) {
+      return interaction.reply({
+        content: "❌ You need to use one of the arguments to select the counting channel.",
+        ephemeral: true,
+      });
+    }
+
+    if (!document.channels.has(channel)) {
+      return interaction.reply({
+        content: "❌ This channel is not a linked counting channel.",
+        ephemeral: true,
+      });
+    }
+
+    document.channels.delete(channel);
+    document.safeSave();
+
+    return interaction.reply({
+      content: `✅ Successfully unlinked counting channel <#${channel}>.`,
+      ephemeral: ephemeralPreference,
+    });
+  },
+} as SlashCommand;
