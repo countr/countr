27e31241b511433f60f69413e62760c172924072
Promise <me@promise.solutions>
2021-12-27 00:54:39 +0100

    Add `/select` command for selecting counting channel

---
+++ src/commands/slash/select.ts
@@ -0,0 +1,31 @@
+import { defaultExpirationValue, selectedCountingChannels } from "../../constants/selectedCountingChannels";
+import { SlashCommand } from "../../types/command";
+
+export default {
+  description: "Select a counting channel to edit",
+  options: [
+    {
+      type: "CHANNEL",
+      name: "channel",
+      description: "The channel to select",
+      channelTypes: ["GUILD_TEXT", "GUILD_PRIVATE_THREAD", "GUILD_PUBLIC_THREAD"],
+      required: true,
+    },
+  ],
+  execute: (interaction, _, { channel }: { channel: string; }, document) => {
+    if (!document.channels.has(channel)) {
+      return interaction.reply({
+        content: "❌ That channel isn't linked to Countr.",
+        ephemeral: true,
+      });
+    }
+
+    const expires = Date.now() + defaultExpirationValue;
+    selectedCountingChannels.set([interaction.guildId, interaction.user.id].join("."), { channel, expires });
+
+    interaction.reply({
+      content: `✅ Selected counting channel <#${channel}> (selection expires <t:${Math.round(expires / 1000)}:R>)`,
+      ephemeral: true,
+    });
+  },
+} as SlashCommand;
