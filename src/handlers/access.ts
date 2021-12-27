5029488d8b11d8005e3a5a271ab494cc6f18c35b
Promise <me@promise.solutions>
2021-12-27 17:42:11 +0100

    Log on leaving guild for missing access

--- src/handlers/access.ts
+++ src/handlers/access.ts
@@ -1,6 +1,7 @@
 import * as access from "../database/access";
 import { Client, Guild, WebhookClient } from "discord.js";
 import config from "../config";
+import { countrLogger } from "../utils/logger/countr";
 
 const webhook = config.access.webhookLog ? new WebhookClient({ url: config.access.webhookLog }) : null;
 
@@ -20,5 +21,6 @@ function leave(guild: Guild): Promise<Guild> {
   webhook?.send({
     content: `Server **${guild.name}** (\`${guild.id}\`) owned by <@${guild.ownerId}> does not have access to use \`${guild.client?.user?.tag}\` and has left the server`,
   });
+  countrLogger.info(`Left server "${guild.name}" owned by ${guild.ownerId} for not having access`);
   return guild.leave();
 }
