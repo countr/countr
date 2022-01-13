c7b7a13295cf9762c474d264283c5dd45f55ca8e
Promise <me@promise.solutions>
2022-01-13 23:52:08 +0100

    build my own roman numeral functions as roman numerals are only up to 3999 numbers lol

--- src/constants/numberSystems.ts
+++ src/constants/numberSystems.ts
@@ -1,4 +1,3 @@
-import { toArabic, toRoman } from "roman-numerals";
 import { evaluate } from "mathjs";
 
 const numberSystems: {
@@ -34,15 +33,11 @@ const numberSystems: {
   roman: {
     name: "Roman (I, II, III, IV, V...)",
     convert: string => {
-      try {
-        const arabic = toArabic(string) || null;
-        if (!arabic || toRoman(arabic) !== string) return null;
-        return arabic;
-      } catch (e) {
-        return null;
-      }
+      const converted = fromRoman(string) || null;
+      if (!converted || toRoman(converted) !== string) return null;
+      return converted;
     },
-    format: toRoman,
+    format: number => toRoman(number) || "0",
   },
   math: {
     name: "Math Expression (4*4 = 16)",
@@ -58,3 +53,41 @@ const numberSystems: {
 };
 
 export default numberSystems;
+
+function toRoman(number: number): string {
+  if (number >= 1000) return `${"M".repeat(Math.floor(number / 1000))}${toRoman(number % 1000)}`;
+  if (number >= 900) return `CM${toRoman(number - 900)}`;
+  if (number >= 500) return `D${toRoman(number - 500)}`;
+  if (number >= 400) return `CD${toRoman(number - 400)}`;
+  if (number >= 100) return `C${toRoman(number - 100)}`;
+  if (number >= 90) return `XC${toRoman(number - 90)}`;
+  if (number >= 50) return `L${toRoman(number - 50)}`;
+  if (number >= 40) return `XL${toRoman(number - 40)}`;
+  if (number >= 10) return `X${toRoman(number - 10)}`;
+  if (number >= 9) return "IX";
+  if (number >= 5) return `V${toRoman(number - 5)}`;
+  if (number >= 4) return "IV";
+  if (number >= 1) return `I${toRoman(number - 1)}`;
+  return "";
+}
+
+function fromRoman(string: string): number {
+  if (string.startsWith("M")) {
+    const firstNonM = string.split("").findIndex(char => char !== "M");
+    if (firstNonM === -1) return 1000 * string.length;
+    return 1000 * firstNonM + fromRoman(string.slice(firstNonM));
+  }
+  if (string.startsWith("CM")) return 900 + fromRoman(string.substring(2));
+  if (string.startsWith("D")) return 500 + fromRoman(string.substring(1));
+  if (string.startsWith("CD")) return 400 + fromRoman(string.substring(2));
+  if (string.startsWith("C")) return 100 + fromRoman(string.substring(1));
+  if (string.startsWith("XC")) return 90 + fromRoman(string.substring(2));
+  if (string.startsWith("L")) return 50 + fromRoman(string.substring(1));
+  if (string.startsWith("XL")) return 40 + fromRoman(string.substring(2));
+  if (string.startsWith("X")) return 10 + fromRoman(string.substring(1));
+  if (string.startsWith("IX")) return 9 + fromRoman(string.substring(2));
+  if (string.startsWith("V")) return 5 + fromRoman(string.substring(1));
+  if (string.startsWith("IV")) return 4 + fromRoman(string.substring(2));
+  if (string.startsWith("I")) return 1 + fromRoman(string.substring(1));
+  return 0;
+}
