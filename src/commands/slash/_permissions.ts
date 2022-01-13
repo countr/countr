dd5e4904e525256af0536be26350168bf9ae0f97
Promise <me@promise.solutions>
2022-01-13 21:32:34 +0100

    add permission for filters

--- src/commands/slash/_permissions.ts
+++ src/commands/slash/_permissions.ts
@@ -3,6 +3,7 @@ import { CommandPermissionLevel } from "../../constants/permissions";
 const permissions: Record<string, CommandPermissionLevel> = {
   channels: "ADMIN",
   data: "OWNER",
+  filters: "MOD",
   flows: "ADMIN",
   modules: "ADMIN",
 };
