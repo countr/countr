51f570390c402c041a2329b1fed63d0660a2cfe1
Promise <me@promise.solutions>
2022-01-19 00:51:37 +0100

    add invite link to satisfy daddy eslint

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 import config from "../config";
 
-export const inviteUrl = `https://todo?client_id=${config.client.id}`; // todo
+export const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${config.client.id}&permissions=327222946832&scope=applications.commands%20bot`;
 
 // documentation
 export const docsUrl = "https://countr.xyz/docs";
