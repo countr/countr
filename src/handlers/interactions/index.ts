8d7c2541dd96fc2e13fb47df08297040df46953e
Promise <me@promise.solutions>
2022-02-27 21:14:04 +0100

    Handle premium commands when it's not premium

--- src/handlers/interactions/index.ts
+++ src/handlers/interactions/index.ts
@@ -1,4 +1,5 @@
 import { ApplicationCommandData, ApplicationCommandOption, ApplicationCommandOptionData, ApplicationCommandSubCommandData, ButtonInteraction, ChatInputApplicationCommandData, Client, CommandInteraction, ContextMenuInteraction, MessageApplicationCommandData, SelectMenuInteraction, UserApplicationCommandData } from "discord.js";
+import { Command } from "../../@types/command";
 import autocompleteHandler from "./autocompletes";
 import commandHandler from "./commands";
 import componentHandler from "./components";
@@ -12,7 +13,10 @@ import { join } from "path";
 export default async (client: Client): Promise<void> => {
   const commands = config.guild ? client.guilds.cache.get(config.guild)?.commands : client.application?.commands;
   if (config.cluster.shards.includes(0)) {
-    commands?.set([...await nestCommands("../../commands/slash", "CHAT_INPUT") as Array<ApplicationCommandData>, ...await nestCommands("../../commands/user", "USER") as Array<ApplicationCommandData>]);
+    commands?.set([
+      ...await nestCommands("../../commands/slash", "CHAT_INPUT") as Array<ApplicationCommandData>,
+      ...await nestCommands("../../commands/user", "USER") as Array<ApplicationCommandData>,
+    ]);
   }
 
   client.on("interactionCreate", async interaction => {
@@ -43,40 +47,42 @@ function nestCommands(relativePath: string, type: string): Promise<Array<Applica
         for (const file of files) {
           if (file.endsWith(".js") && !file.startsWith("_")) {
 
-            const { description, options }: {
-          description?: string;
-          options?: Array<ApplicationCommandOption>;
-        } = (await import(`${relativePath}/${file}`)).default;
+            const { description, options, premiumOnly }: {
+              description?: string;
+              options?: Array<ApplicationCommandOption>;
+            } & Command = (await import(`${relativePath}/${file}`)).default;
 
-            if (type === "USER") {
-              arr.push({
-                type: "USER",
-                name: file.replace(".js", ""),
-              } as UserApplicationCommandData);
-            } else if (type === "MESSAGE") {
-              arr.push({
-                type: "MESSAGE",
-                name: file.replace(".js", ""),
-              } as MessageApplicationCommandData);
-            } else if (type === "CHAT_INPUT") {
-              arr.push({
-                type: "CHAT_INPUT",
-                name: file.replace(".js", ""),
-                description: description || "No description",
-                options: options || [],
-              } as ChatInputApplicationCommandData);
-            } else if (type === "SUB_COMMAND") {
-              arr.push(({
-                type: "SUB_COMMAND",
-                name: file.replace(".js", ""),
-                description: description || "No description",
-                options: options || [],
-              } as ApplicationCommandOptionData & ApplicationCommandSubCommandData) as ApplicationCommandOption);
+            if (!premiumOnly || config.isPremium) {
+              if (type === "USER") {
+                arr.push({
+                  type: "USER",
+                  name: file.replace(".js", ""),
+                } as UserApplicationCommandData);
+              } else if (type === "MESSAGE") {
+                arr.push({
+                  type: "MESSAGE",
+                  name: file.replace(".js", ""),
+                } as MessageApplicationCommandData);
+              } else if (type === "CHAT_INPUT") {
+                arr.push({
+                  type: "CHAT_INPUT",
+                  name: file.replace(".js", ""),
+                  description: description || "No description",
+                  options: options || [],
+                } as ChatInputApplicationCommandData);
+              } else if (type === "SUB_COMMAND") {
+                arr.push(({
+                  type: "SUB_COMMAND",
+                  name: file.replace(".js", ""),
+                  description: description || "No description",
+                  options: options || [],
+                } as ApplicationCommandOptionData & ApplicationCommandSubCommandData) as ApplicationCommandOption);
+              }
             }
 
           } else if (!file.includes(".")) {
             const options = await nestCommands(`${relativePath}/${file}`, "SUB_COMMAND") as Array<ApplicationCommandOption>;
-            arr.push({ name: file, description: "Sub-command.", options, type: type === "SUB_COMMAND" ? "SUB_COMMAND_GROUP" : type } as ChatInputApplicationCommandData);
+            if (options.length) arr.push({ name: file, description: "Sub-command.", options, type: type === "SUB_COMMAND" ? "SUB_COMMAND_GROUP" : type } as ChatInputApplicationCommandData);
           }
         }
       }
