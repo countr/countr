7593f59dcd999b1c0c6815ff13c886c0fc014e75
Promise <me@promise.solutions>
2022-02-27 20:27:52 +0100

    Refactor constant links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,17 +1,18 @@
 import config from "../config";
 
-export const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${config.client.id}&permissions=327222946832&scope=applications.commands%20bot`;
+export const homepage = "https://countr.xyz";
 
 // documentation
-export const docsUrl = "https://countr.xyz/docs";
-export const cacheHelpUrl = "https://countr.xyz/caching";
-export const flowHelpUrl = "https://countr.xyz/flows";
-export const regexHelpUrl = "https://countr.xyz/regex";
-export const premiumHelpUrl = "https://countr.xyz/premium";
+export const docsUrl = `${homepage}/docs`;
+export const cacheHelpUrl = `${homepage}/caching`;
+export const flowHelpUrl = `${homepage}/flows`;
+export const regexHelpUrl = `${homepage}/regex`;
+export const premiumHelpUrl = `${homepage}/premium`;
 
 // miscellaneous
+export const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${config.client.id}&permissions=327222946832&scope=applications.commands%20bot`;
 export const supportServerUrl = "https://promise.solutions/discord";
-export const privacyUrl = "https://countr.xyz/privacy";
-export const termsUrl = "https://countr.xyz/terms";
+export const privacyUrl = `${homepage}/privacy`;
+export const termsUrl = `${homepage}/terms`;
 export const sourceUrl = "https://github.com/countr";
 export const uptimeUrl = "https://uptime.countr.xyz";
