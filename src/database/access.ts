b46df1e46a95514219961b4809d4dd317947f293
Promise <me@promise.solutions>
2022-07-10 18:22:53 +0200

    Update Access database model

--- src/database/access.ts
+++ src/database/access.ts
@@ -2,10 +2,10 @@ import { Access } from "./models/Access";
 import type { Snowflake } from "discord.js";
 
 export function checkGuildAccess(guildId: Snowflake): Promise<boolean> {
-  return Access.findOne({ servers: guildId, expires: { $gt: new Date() }}).then(Boolean);
+  return Access.findOne({ guildIds: guildId, expires: { $gt: new Date() }}).then(Boolean);
 }
 
 export async function filterGuildsWithAccess(guildIds: Snowflake[]): Promise<Snowflake[]> {
-  const accessDocuments = await Access.find({ servers: { $in: guildIds }, expires: { $gt: new Date() }});
-  return guildIds.filter(guildId => accessDocuments.some(accessDocument => accessDocument.servers.includes(guildId)));
+  const accessDocuments = await Access.find({ guildIds: { $in: guildIds }, expires: { $gt: new Date() }});
+  return guildIds.filter(guildId => accessDocuments.some(accessDocument => accessDocument.guildIds.includes(guildId)));
 }
--- src/database/models/Access.ts
+++ src/database/models/Access.ts
@@ -1,11 +1,14 @@
+// should be the same as this file from the countr/access-manager repository:
+// https://github.com/countr/access-manager/blob/djs-14/src/database/models/Access.ts
+
 import { getModelForClass, prop } from "@typegoose/typegoose";
 import type { DocumentType } from "@typegoose/typegoose";
+import { PropType } from "@typegoose/typegoose/lib/internal/constants";
 import type { Snowflake } from "discord.js";
-import { WhatIsIt } from "@typegoose/typegoose/lib/internal/constants";
 
 export class AccessSchema {
-  @prop({ type: String, required: true }) user!: string;
-  @prop({ type: [String], default: []}, WhatIsIt.ARRAY) servers!: Snowflake[];
+  @prop({ type: String, required: true }) userId!: string;
+  @prop({ type: [String], default: []}, PropType.ARRAY) guildIds!: Snowflake[];
   @prop({ type: Date, required: true }) expires!: Date;
 }
 
