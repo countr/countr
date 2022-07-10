267e7ca980946e8f3df1c185d49ecaa5af884cbf
Promise <me@promise.solutions>
2022-07-10 18:18:51 +0200

    Add `/slowmode` command

---
+++ src/commands/chatInput/set/slowmode.ts
@@ -0,0 +1,25 @@
+import { ApplicationCommandOptionType } from "discord.js";
+import type { ChatInputCommand } from "..";
+
+const command: ChatInputCommand = {
+  description: "Set the slowmode",
+  considerDefaultPermission: false,
+  options: [
+    {
+      type: ApplicationCommandOptionType.Integer,
+      name: "seconds",
+      description: "The amount to set the slowmode to",
+      required: true,
+    },
+  ],
+  requireSelectedCountingChannel: true,
+  execute(interaction, ephemeral, _, [countingChannelId]) {
+    const rateLimitPerUser = interaction.options.getInteger("seconds", true);
+
+    return void interaction.guild.channels.resolve(countingChannelId)?.edit({ rateLimitPerUser })
+      .then(() => interaction.reply({ content: `✅ Counting channel <#${countingChannelId}>'s slowmode is now set to ${rateLimitPerUser} seconds.`, ephemeral }))
+      .catch(() => interaction.reply({ content: `❌ Failed to set slowmode to ${rateLimitPerUser} seconds. Are you sure this is a valid amount of seconds for slowmode?`, ephemeral }));
+  },
+};
+
+export default { ...command } as ChatInputCommand;
