828b447df800503af036de437fa627d2ee52ab46
Promise <me@promise.solutions>
2022-08-04 13:53:27 +0200

    Export pruned scores as a backup

--- src/commands/chatInput/scores/prune.ts
+++ src/commands/chatInput/scores/prune.ts
@@ -1,4 +1,5 @@
 import type { ChatInputCommand } from "..";
+import type { Snowflake } from "discord.js";
 
 const command: ChatInputCommand = {
   description: "Prune scores from users that have left your server",
@@ -11,15 +12,22 @@ const command: ChatInputCommand = {
 
     let amount = 0;
     const total = countingChannel.scores.size;
+    const backup: Record<Snowflake, number> = {};
+
     countingChannel.scores.forEach((_, userId) => {
       if (!members.has(userId)) {
+        backup[userId] = countingChannel.scores.get(userId) ?? 0;
         countingChannel.scores.delete(userId);
         amount += 1;
       }
     });
+
     document.safeSave();
 
-    return void interaction.editReply(`✅ Successfully pruned scores of counting channel <#${countingChannelId}>: removed ${amount}/${total} users from the list.`);
+    return void interaction.editReply({
+      content: `✅ Successfully pruned scores of counting channel <#${countingChannelId}>: removed ${amount}/${total} users from the list. Attached is a backup of their scores, in case this was a mistake.`,
+      files: [{ name: `Countr Pruned Scores of guild ${interaction.guildId} channel ${interaction.channelId}.json`, attachment: Buffer.from(JSON.stringify(backup, null, 2)) }],
+    });
   },
 };
 
