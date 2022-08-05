bfa88e2dba037fb37f6d5bd23229fb640fb18915
Promise <me@promise.solutions>
2022-08-05 12:13:14 +0200

    Fix webhook module

--- src/handlers/counting/repost/webhook.ts
+++ src/handlers/counting/repost/webhook.ts
@@ -1,32 +1,32 @@
-import type { GuildMember, Message, Webhook } from "discord.js";
-import type { CountingChannelAllowedChannelType } from "../../../constants/discord";
+import type { CountingChannelAllowedChannelType, CountingChannelRootChannel } from "../../../constants/discord";
+import type { GuildMember, Message, Snowflake, Webhook } from "discord.js";
 import { TextChannel } from "discord.js";
 
-const webhookCache = new Map<TextChannel, Webhook>();
+const webhookCache = new Map<Snowflake, Webhook>();
 
 export default async function repostWithWebhook(message: Message<true>, member: GuildMember, secondTry = false): Promise<Message> {
   const channel = message.channel as CountingChannelAllowedChannelType;
 
   // if the channel is a thread then get the parent channel
-  const textChannel = channel instanceof TextChannel ? channel : channel.parent as TextChannel;
+  const textChannel = channel instanceof TextChannel ? channel : channel.parent as CountingChannelRootChannel;
 
-  let webhook = webhookCache.get(textChannel);
+  let webhook = webhookCache.get(textChannel.id);
   if (!webhook) {
     const webhooks = await textChannel.fetchWebhooks();
-    webhook = webhooks.find(wh => wh.name === "Countr") ?? await textChannel.createWebhook({ name: "Countr", avatar: message.client.user?.avatar ?? null });
-    webhookCache.set(textChannel, webhook);
+    webhook = webhooks.find(wh => wh.name === "Countr") ?? await textChannel.createWebhook({ name: "Countr", avatar: message.guild.members.me?.displayAvatarURL({ forceStatic: false, size: 64 }) ?? null });
+    webhookCache.set(textChannel.id, webhook);
   }
 
   return webhook.send({
     content: message.content,
     username: member.displayName,
-    avatarURL: member.avatarURL({ forceStatic: false, size: 64 }) ?? member.user.defaultAvatarURL,
+    avatarURL: member.displayAvatarURL({ forceStatic: false, size: 64 }),
     allowedMentions: { parse: [], roles: [], users: []},
     ...channel.isThread() && { threadId: channel.id },
   }).catch(() => {
     // if it fails then assume the webhook is broken and delete it. if it's the second try however, just skip it.
     if (secondTry) return message;
-    webhookCache.delete(textChannel);
+    webhookCache.delete(textChannel.id);
     return repostWithWebhook(message, member, true);
   });
 }
