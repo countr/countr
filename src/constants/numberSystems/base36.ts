971fb6baf31bd9d67a83822b01573fc64c45b184
Glenn <me@promise.solutions>
2022-10-01 09:57:53 +0000

    Make hex and base64 counting case insensitive

--- src/constants/numberSystems/base36.ts
+++ src/constants/numberSystems/base36.ts
@@ -4,7 +4,7 @@ const base36: NumberSystem = {
   name: "Base36 (36-number system using 0-9 and A-Z)",
   convert: input => {
     const converted = parseInt(input, 36);
-    if (isNaN(converted) || base36.format(converted) !== input) return null;
+    if (isNaN(converted) || base36.format(converted).toLowerCase() !== input.toLowerCase()) return null;
     return converted;
   },
   format: number => number.toString(36),
