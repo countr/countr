11383afe26acb1009ef3fcf505ee39411a1fbcc8
Talal <talalahmad257@gmail.com>
2022-10-13 14:46:33 +0500

    Revert changes
    Signed-off-by: Talal <talalahmad257@gmail.com>

--- src/handlers/counting/repost/embed.ts
+++ src/handlers/counting/repost/embed.ts
@@ -2,12 +2,11 @@ import type { Message } from "discord.js";
 
 export default async function repostWithEmbed(message: Message<true>): Promise<Message> {
   try {
-    const { displayColor } = await message.guild.members.fetch(message.author);
     return await message.channel.send({
       embeds: [
         {
           description: `${message.author.toString()}: ${message.content}`,
-          color: displayColor || 3092790,
+          color: message.member?.displayColor ?? 3092790,
         },
       ],
     });
