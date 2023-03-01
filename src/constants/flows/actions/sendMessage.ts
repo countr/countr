227f94c3fdcea1b527c6a9cc4d04201f431f97bd
Talal <talalahmad257@gmail.com>
2023-03-01 05:26:58 +0000

    lint

--- src/constants/flows/actions/sendMessage.ts
+++ src/constants/flows/actions/sendMessage.ts
@@ -1,17 +1,16 @@
+import { cleanContent, escapeCodeBlock } from "discord.js";
 import type { Action } from ".";
 import type { Snowflake } from "discord.js";
-import { escapeCodeBlock, cleanContent } from "discord.js";
 import properties from "../../properties";
 
 const sendMessage: Action<[Snowflake, string]> = {
   name: "Send a message",
   description: "This will send a message in any channel you'd like",
   properties: [properties.channel, properties.text],
-  explanation: ([channel, text]) =>
-    `Send a message in <#${channel}>: \`\`\`${escapeCodeBlock(text)}\`\`\``,
+  explanation: ([channel, text]) => `Send a message in <#${channel}>: \`\`\`${escapeCodeBlock(text)}\`\`\``,
   run: async (
-    { count, member, message: { guild, content }, countingChannel: { scores } },
-    [channelId, text]
+    { count, member, message: { guild, content }, countingChannel: { scores }},
+    [channelId, text],
   ) => {
     const channel = guild.channels.resolve(channelId);
     if (channel?.isTextBased()) {
@@ -26,12 +25,12 @@ const sendMessage: Action<[Snowflake, string]> = {
             .replace(/\{everyone\}/giu, guild.roles.everyone.toString())
             .replace(/\{score\}/giu, String(scores.get(member.id) ?? 0))
             .replace(/\{content\}/giu, cleanContent(content, channel)),
-          allowedMentions: { parse: ["everyone", "users", "roles"] }
+          allowedMentions: { parse: ["everyone", "users", "roles"]},
         })
         .catch();
     }
     return false;
-  }
+  },
 };
 
 export default sendMessage;
