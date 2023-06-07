d678a41cc0d0d51cb155f4403ec9cf9b1c1cf05b
Promise <me@promise.solutions>
2023-06-07 09:38:38 +0000

    feat: channel and role select menus
    Signed-off-by: Promise <me@promise.solutions>

--- src/constants/properties/inputs/channel.ts
+++ src/constants/properties/inputs/channel.ts
@@ -1,35 +1,35 @@
 import type { Snowflake } from "discord.js";
-import { TextInputStyle } from "discord.js";
-import { matchSorter } from "match-sorter";
-import { createModalTextInput, getModalTextInput, modals } from "../../../handlers/interactions/modals";
+import { ComponentType } from "discord.js";
+import { selectMenuComponents } from "../../../handlers/interactions/components";
+import { textBasedChannelTypes } from "../../discord";
 import type { PropertyInput } from ".";
 
-const channelInput: PropertyInput<Snowflake> = (interaction, currentValue) => new Promise(resolve => {
-  // edit this to select menu whenever it comes out
-  void interaction.showModal({
-    title: "Search channel name or ID",
-    customId: `${interaction.id}:modalSubmit`,
+const channelInput: PropertyInput<Snowflake> = interaction => new Promise(resolve => {
+  void interaction.update({
+    content: "🔻 What channel would you like to use?",
     components: [
-      createModalTextInput({
-        style: TextInputStyle.Short,
-        customId: "query",
-        label: "Write a channel name or ID here",
-        minLength: 1,
-        maxLength: 100,
-        required: true,
-        ...currentValue && { value: currentValue },
-      }),
+      {
+        type: ComponentType.ActionRow,
+        components: [
+          {
+            type: ComponentType.ChannelSelect,
+            customId: `${interaction.id}:select`,
+            placeholder: "Select a channel",
+            channelTypes: [...textBasedChannelTypes],
+            minValues: 1,
+            maxValues: 1,
+          },
+        ],
+      },
     ],
   });
 
-  modals.set(`${interaction.id}:modalSubmit`, {
-    callback(modalInteraction) {
-      const query = getModalTextInput(modalInteraction.components, "query");
-      const channel = query && (
-        interaction.guild.channels.resolve(query) ||
-        matchSorter(interaction.guild.channels.cache.map(({ name, id }) => ({ name, id })), query, { keys: ["name"] })[0]) ||
-        null;
-      return resolve([channel?.id ?? null, modalInteraction]);
+  selectMenuComponents.set(`${interaction.id}:select`, {
+    selectType: "channel",
+    allowedUsers: [interaction.user.id],
+    callback(selectInteraction) {
+      const [channelId] = selectInteraction.values as [Snowflake];
+      return resolve([channelId || null, selectInteraction]);
     },
   });
 });
--- src/constants/properties/inputs/roles.ts
+++ src/constants/properties/inputs/roles.ts
@@ -1,38 +1,33 @@
 import type { Snowflake } from "discord.js";
-import { TextInputStyle } from "discord.js";
-import { matchSorter } from "match-sorter";
-import { createModalTextInput, getModalTextInput, modals } from "../../../handlers/interactions/modals";
+import { ComponentType } from "discord.js";
+import { selectMenuComponents } from "../../../handlers/interactions/components";
 import type { PropertyInput } from ".";
 
-const roleInput: PropertyInput<Snowflake[]> = (interaction, currentValue) => new Promise(resolve => {
-  // edit this to select menu whenever it comes out
-  void interaction.showModal({
-    title: "Search role names or IDs",
-    customId: `${interaction.id}:modalSubmit`,
+const roleInput: PropertyInput<Snowflake[]> = interaction => new Promise(resolve => {
+  void interaction.update({
+    content: "🔻 What roles would you like to use?",
     components: [
-      createModalTextInput({
-        style: TextInputStyle.Paragraph,
-        customId: "query",
-        label: "Separate multiple with new line",
-        minLength: 1,
-        required: true,
-        ...currentValue?.length && { value: currentValue.join("\n") },
-      }),
+      {
+        type: ComponentType.ActionRow,
+        components: [
+          {
+            type: ComponentType.RoleSelect,
+            customId: `${interaction.id}:select`,
+            placeholder: "Select roles",
+            minValues: 1,
+            maxValues: 10,
+          },
+        ],
+      },
     ],
   });
 
-  modals.set(`${interaction.id}:modalSubmit`, {
-    callback(modalInteraction) {
-      const query = String(getModalTextInput(modalInteraction.components, "query")).split("\n");
-      const roles: Snowflake[] = [];
-      for (const roleQuery of query) {
-        const role = roleQuery && (
-          interaction.guild.roles.resolve(roleQuery) ||
-          matchSorter(interaction.guild.roles.cache.map(({ name, id }) => ({ name, id })), roleQuery, { keys: ["name"] })[0]
-        ) || null;
-        if (role) roles.push(role.id);
-      }
-      return resolve([roles.length ? roles : null, modalInteraction]);
+  selectMenuComponents.set(`${interaction.id}:select`, {
+    selectType: "role",
+    allowedUsers: [interaction.user.id],
+    callback(selectInteraction) {
+      const roleIds = selectInteraction.values;
+      return resolve([roleIds.length ? roleIds : null, selectInteraction]);
     },
   });
 });
