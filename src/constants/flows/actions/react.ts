b05b450c711452cc2aa52268f7c7b912183068a2
Glenn <me@promise.solutions>
2023-06-29 10:30:45 +0200

    feat: opt-in to ignore failed messages & new react action (#848)
    * feat(modules): opt-in to not delete failed counts
    
    Signed-off-by: Promise <me@promise.solutions>
    
    * feat(actions): react to a counting message
    
    Signed-off-by: Promise <me@promise.solutions>
    
    * fix: update react action description
    
    Signed-off-by: Promise <me@promise.solutions>
    
    ---------
    
    Signed-off-by: Promise <me@promise.solutions>

---
+++ src/constants/flows/actions/react.ts
@@ -0,0 +1,20 @@
+import { inspect } from "util";
+import countingLogger from "../../../utils/logger/counting";
+import properties from "../../properties";
+import type { Action } from ".";
+
+const react: Action<[string]> = {
+  name: "Message reaction",
+  description: "This will react to the counting message with an emoji. It also supports the `nodelete` module for failed counts.",
+  properties: [properties.emoji],
+  explanation: ([emoji]) => `React with ${emoji} to the counting message`,
+  run: async ({ countingMessage }, [emoji]) => {
+    await countingMessage.react(emoji).catch(err => {
+      countingLogger.error(`Failed to react to counting message ${countingMessage.id}, channel ${countingMessage.channel.id}, guild ${countingMessage.guild!.id}, member ${countingMessage.author.id}, with the emoji ${emoji}: ${inspect(err)}`);
+    });
+    return false;
+  },
+  limitPerFlow: 1,
+};
+
+export default react;
--- src/constants/properties/index.ts
+++ src/constants/properties/index.ts
@@ -2,6 +2,7 @@ import type { Schema } from "ajv";
 import type { Awaitable, Guild } from "discord.js";
 import anyNumber from "./anyNumber";
 import channel from "./channel";
+import emoji from "./emoji";
 import type { PropertyInput } from "./inputs";
 import numberPositive from "./numberPositive";
 import numberPositiveOrNegative from "./numberPositiveOrNegative";
@@ -13,7 +14,7 @@ import text from "./text";
 export type PropertyValue = PropertyValue[] | number | string;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export interface Property<T extends PropertyValue = any, U extends PropertyValue = T> {
+export interface Property<T extends PropertyValue = any, U = T> {
   name: string;
   description?: string;
   schema: Schema;
@@ -22,4 +23,4 @@ export interface Property<T extends PropertyValue = any, U extends PropertyValue
   format(converted: T, guild: Guild): Awaitable<string>;
 }
 
-export default { anyNumber, channel, numberPositive, numberPositiveOrNegative, numberPositiveOrZero, regex, roles, text } as const;
+export default { anyNumber, channel, emoji, numberPositive, numberPositiveOrNegative, numberPositiveOrZero, regex, roles, text } as const;
