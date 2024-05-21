8bfe7fc5f06f4c8472c1dfcd35ca9fc163db3749
Talal <mail@ta1al.com>
2024-05-21 14:46:14 +0500

    fix: Convert raw number to appropriate type before resending deleted msgs (#1251)
    * Convert raw number to appropriate type
    
    Fixed #1250 
    
    Signed-off-by: Talal <mail@ta1al.com>
    
    * fix: lint
    
    * fix: type casting
    
    * fix: use format instead of convert
    
    ---------
    
    Signed-off-by: Talal <mail@ta1al.com>

--- src/handlers/counting/replacement.ts
+++ src/handlers/counting/replacement.ts
@@ -1,11 +1,13 @@
 import type { Message, PartialMessage } from "discord.js";
+import numberSystems from "../../constants/numberSystems";
 import type { CountingChannelSchema, GuildDocument } from "../../database/models/Guild";
 
 export default async function replaceUpdatedOrDeletedMessage(message: Message | PartialMessage, document: GuildDocument, countingChannel: CountingChannelSchema, isDeleted: boolean): Promise<void> {
   const ownerId = message.author?.id ?? message.member?.id ?? countingChannel.count.userId ?? null;
   if (ownerId !== countingChannel.count.userId) return;
 
-  const newMessage = await message.channel.send(`*<@${ownerId}>: ${countingChannel.count.number}*`);
+  const newNumber = numberSystems[countingChannel.type].format(countingChannel.count.number);
+  const newMessage = await message.channel.send(`*<@${ownerId}>: ${newNumber}*`);
   if (countingChannel.count.messageId === message.id) {
     countingChannel.count.messageId = newMessage.id;
     document.safeSave();
