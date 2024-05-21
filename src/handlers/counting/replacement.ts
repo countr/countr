153f7083ca9aa8b96fda50b28945a065f8a63a4e
Talal <mail@ta1al.com>
2024-05-21 14:24:45 +0500

    Convert raw number to appropriate type
    Fixed #1250 
    
    Signed-off-by: Talal <mail@ta1al.com>

--- src/handlers/counting/replacement.ts
+++ src/handlers/counting/replacement.ts
@@ -1,11 +1,12 @@
 import type { Message, PartialMessage } from "discord.js";
 import type { CountingChannelSchema, GuildDocument } from "../../database/models/Guild";
+import numberSystems from "../../constants/numberSystems";
 
 export default async function replaceUpdatedOrDeletedMessage(message: Message | PartialMessage, document: GuildDocument, countingChannel: CountingChannelSchema, isDeleted: boolean): Promise<void> {
   const ownerId = message.author?.id ?? message.member?.id ?? countingChannel.count.userId ?? null;
   if (ownerId !== countingChannel.count.userId) return;
 
-  const newMessage = await message.channel.send(`*<@${ownerId}>: ${countingChannel.count.number}*`);
+  const newMessage = await message.channel.send(`*<@${ownerId}>: ${numberSystems[countingChannel.type].convert(countingChannel.count.number)}*`);
   if (countingChannel.count.messageId === message.id) {
     countingChannel.count.messageId = newMessage.id;
     document.safeSave();
