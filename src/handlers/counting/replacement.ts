76631965399318b0d46472a4db889282e5f7625f
Talal <talalahmad257@gmail.com>
2024-05-21 14:32:41 +0500

    fix: lint

--- src/handlers/counting/replacement.ts
+++ src/handlers/counting/replacement.ts
@@ -1,12 +1,13 @@
 import type { Message, PartialMessage } from "discord.js";
-import type { CountingChannelSchema, GuildDocument } from "../../database/models/Guild";
 import numberSystems from "../../constants/numberSystems";
+import type { CountingChannelSchema, GuildDocument } from "../../database/models/Guild";
 
 export default async function replaceUpdatedOrDeletedMessage(message: Message | PartialMessage, document: GuildDocument, countingChannel: CountingChannelSchema, isDeleted: boolean): Promise<void> {
   const ownerId = message.author?.id ?? message.member?.id ?? countingChannel.count.userId ?? null;
   if (ownerId !== countingChannel.count.userId) return;
 
-  const newMessage = await message.channel.send(`*<@${ownerId}>: ${numberSystems[countingChannel.type].convert(countingChannel.count.number)}*`);
+  const newNumber = numberSystems[countingChannel.type].convert(toString(countingChannel.count.number));
+  const newMessage = await message.channel.send(`*<@${ownerId}>: ${newNumber}*`);
   if (countingChannel.count.messageId === message.id) {
     countingChannel.count.messageId = newMessage.id;
     document.safeSave();
